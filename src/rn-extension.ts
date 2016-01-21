9cf5eef212122b370d9c6b3e7a690dcda708e67a
leolee <leolee@microsoft.com>
2016-01-20 23:15:04 -0800

    starter project

---
+++ src/rn-extension.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+import * as vscode from 'vscode';
+import {ReactNativeProjectHelper} from './utils/reactNativeProjectHelper';
+import {ReactNativeCommandHelper} from './utils/reactNativeCommandHelper';
+
+export function activate(context: vscode.ExtensionContext): void {
+    // TODO:  Get the project root (vscode.workspace.rootPath) and return if it is not a react-native project
+    // check if package.json of user project has dependency on react-native   
+    
+
+    // Register react native commands
+    context.subscriptions.push(vscode.commands.registerCommand('reactNative.runAndroid',
+        () => ReactNativeCommandHelper.executeReactNativeCommand(vscode.workspace.rootPath, "runAndroid")));
+    context.subscriptions.push(vscode.commands.registerCommand('reactNative.runIos',
+        () => ReactNativeCommandHelper.executeReactNativeCommand(vscode.workspace.rootPath, "runIos")));
+    context.subscriptions.push(vscode.commands.registerCommand('reactNative.startPackager',
+        () => ReactNativeCommandHelper.executeReactNativeCommand(vscode.workspace.rootPath, "startPackager")));
+    context.subscriptions.push(vscode.commands.registerCommand('reactNative.stopPackager',
+        () => ReactNativeCommandHelper.executeReactNativeCommand(vscode.workspace.rootPath, "stopPackager")));
+    
+}
---
+++ src/utils/reactNativeCommandHelper.ts
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import * as child_process from 'child_process';
+import * as os from 'os';
+import {window} from 'vscode';
+
+export class ReactNativeCommandHelper {
+    private static CORDOVA_CMD_NAME = "react-native";
+
+    public static executeReactNativeCommand(projectRoot: string, command: string) {
+        
+    }
+}
