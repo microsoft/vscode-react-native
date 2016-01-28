e15bc470c1fec84fa709b916f1a81b89a7245c56
unknown <dalebu@DALEBU-SERVER.redmond.corp.microsoft.com>
2016-01-28 15:33:05 -0800

    Renamed AppStrategy to DebugStrategy.

--- src/debugger/launcher.ts
+++ src/debugger/launcher.ts
@@ -6,7 +6,7 @@ import {Package} from "../utils/node/package";
 import {Packager} from "./packager";
 
 export class Launcher {
-    private appStrategy: IDebugStrategy;
+    private debugStrategy: IDebugStrategy;
     private projectRootPath: string;
 
     constructor(projectRootPath: string) {
@@ -22,17 +22,17 @@ export class Launcher {
         return new Package(this.projectRootPath);
     }
 
-    private createAppStrategy(): IDebugStrategy {
+    private createDebugStrategy(): IDebugStrategy {
         let platform = this.appPlatform();
         switch (platform) {
             // We lazyly load the strategies, because some components might be
             // missing on some platforms (like XCode in Windows)
             case "ios":
-                let iosAppStrategy = require("./ios/appStrategy");
-                return new iosAppStrategy.AppStrategy(this.reactNativeProject());
+                let iosDebugStrategy = require("./ios/iosDebugStrategy");
+                return new iosDebugStrategy.IOSDebugStrategy(this.reactNativeProject());
             case "android":
-                let androidAppStrategy = require("./android/appStrategy");
-                return new androidAppStrategy.AppStrategy(this.reactNativeProject());
+                let androidDebugStrategy = require("./android/androidDebugStrategy");
+                return new androidDebugStrategy.AndroidDebugStrategy(this.reactNativeProject());
             default:
                 throw new RangeError("The platform <" + platform + "> is not a valid react-native platform. Accepted platforms are \"iOS\" and \"Android\"");
         }
@@ -52,14 +52,14 @@ export class Launcher {
     }
 
     public launch() {
-        this.appStrategy = this.createAppStrategy();
+        this.debugStrategy = this.createDebugStrategy();
 
         return Q({})
             .then(() => Q.delay(new Packager(this.projectRootPath).start(), 3000))
-            .then(() => Q.delay(this.appStrategy.runApp(), 3000))
+            .then(() => Q.delay(this.debugStrategy.runApp(), 3000))
             .then(() => Q.delay(new DebuggerWorker(this.projectRootPath).start(), 3000)) // Start the worker
-            .then(() => this.appStrategy.enableJSDebuggingMode())
-            .done(() => {}, reason => {
+            .then(() => this.debugStrategy.enableJSDebuggingMode())
+            .done(() => { }, reason => {
                 console.log("Couldn\"t debug react-native app: " + this.printError(reason));
             });
 
