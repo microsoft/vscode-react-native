cb727369b36ef345ef1f57c303067b846e7512fb
dlebu <dalebu@microsoft.com>
2016-02-08 17:06:13 -0800

    Always copy launchReactNative.js.

--- src/rn-extension.ts
+++ src/rn-extension.ts
@@ -53,11 +53,17 @@ try {
 
     let fsUtil = new FileSystem();
 
-    fsUtil.ensureDirectory(vscodeFolder).then(() => {
-        fsUtil.ensureFileWithContents(debugStub, debuggerEntryCode);
-    }).catch((err: Error) => {
-        vscode.window.showErrorMessage(err.message);
-    });
+    fsUtil.ensureDirectory(vscodeFolder)
+        .then(() => fsUtil.pathExists(debugStub))
+        .then((fileExists) => {
+            if (fileExists) {
+                return fsUtil.removePathRecursivelyAsync(debugStub);
+            }
+        })
+        .then(() => fsUtil.ensureFileWithContents(debugStub, debuggerEntryCode))
+        .catch((err: Error) => {
+            vscode.window.showErrorMessage(err.message);
+        });
 }
 
 /**
