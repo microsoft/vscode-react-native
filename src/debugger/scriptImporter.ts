87e1f7764c764578408658c141a3e672fb9ea958
Jimmy Thomson <jithomso@microsoft.com>
2016-02-12 15:54:17 -0800

    Fixing typos and addressing PR feedback

--- src/debugger/scriptImporter.ts
+++ src/debugger/scriptImporter.ts
@@ -47,6 +47,9 @@ export class ScriptImporter {
                 .then((scriptFilePath: string) => {
                     Log.logInternalMessage(LogLevel.Info, `Script ${scriptUrlString} downloaded to ${scriptFilePath}`);
                     return { contents: scriptBody, filepath: scriptFilePath };
+                }).finally(() => {
+                    // Request that the debug adapter update breakpoints and sourcemaps now that we have written them
+                    return new Request().request(`http://localhost:${this.debugAdapterPort}/refreshBreakpoints`);
                 });
         });
     }
@@ -70,10 +73,6 @@ export class ScriptImporter {
                 let scriptFileRelativePath = path.basename(scriptUrl.pathname); // scriptFileRelativePath = "index.ios.bundle"
                 let updatedContent = this.sourceMapUtil.updateSourceMapFile(sourceMapBody, scriptFileRelativePath, this.sourcesStoragePath);
                 return new FileSystem().writeFile(sourceMappingLocalPath, updatedContent);
-            }).then(() => {
-                // TODO: update port based on command line arguments
-                // Request that the debug adapter update breakpoints and sourcemaps now that we have written them
-                return new Request().request(`http://localhost:${this.debugAdapterPort}/refreshBreakpoints`);
             });
     }
 }
