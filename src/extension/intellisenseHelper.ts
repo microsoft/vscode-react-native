701c2cb3746f6250c7ce59518808f4a0ff4e878b
Joshua Skelton <joskelto@microsoft.com>
2016-02-12 15:38:37 -0800

    Removed npm dependency

--- src/extension/intellisenseHelper.ts
+++ src/extension/intellisenseHelper.ts
@@ -2,12 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import {FileSystem} from "../common/node/fileSystem";
+import * as child_process from "child_process";
 import * as os from "os";
-import * as npm from "npm";
 import * as path from "path";
 import * as Q from "q";
 import * as vscode from "vscode";
-import {Node} from "../common/node/node";
 import {TsConfigHelper} from "./tsconfigHelper";
 import {SettingsHelper} from "./settingsHelper";
 
@@ -22,20 +21,18 @@ export class IntellisenseHelper {
                 });
         }
 
-        TsConfigHelper.allowJs(true)
-            .then(function() {
-                return TsConfigHelper.addExcludePaths(["node_modules"]);
-            })
+        Q({})
+            .then(() => TsConfigHelper.allowJs(true))
+            .then(() => TsConfigHelper.addExcludePaths(["node_modules"]))
             .done();
 
         let reactTypingsSource = path.resolve(__dirname, "..", "..", "ReactTypings");
         let reactTypingsDest = path.resolve(vscode.workspace.rootPath, ".vscode", "typings");
         let fileSystem = new FileSystem();
 
-        fileSystem.copyRecursive(reactTypingsSource, reactTypingsDest)
-            .then(function() {
-                return IntellisenseHelper.installTypescriptNext();
-            })
+        Q({})
+            .then(() => fileSystem.copyRecursive(reactTypingsSource, reactTypingsDest))
+            .then(() => IntellisenseHelper.installTypescriptNext())
             .done();
     }
 
@@ -48,12 +45,7 @@ export class IntellisenseHelper {
         return fileSystem.exists(typeScriptNextLibPath)
             .then(function(exists: boolean) {
                 if (!exists) {
-                    return Q({})
-                        .then(() => Q.nfcall(npm.load, {}))
-                        .then(() => {
-                            npm.prefix = typeScriptNextDest;
-                            return Q.ninvoke(npm.commands, "install", ["typescript@next"]);
-                        });
+                    return Q.ninvoke(child_process, "exec", `npm install --prefix ${typeScriptNextDest} typescript@next`);
                 }
             })
             .then(function() {
@@ -63,20 +55,17 @@ export class IntellisenseHelper {
 
     public static enableSalsa(): void {
         if (!process.env.VSCODE_TSJS) {
-            let setSalsaEnvVariable: string = "";
+            let setEnvironmentVariableCommand: string = "";
             if (os.type() === "Darwin") {
-                setSalsaEnvVariable = "launchctl setenv VSCODE_TSJS 1";
+                setEnvironmentVariableCommand = "launchctl setenv VSCODE_TSJS 1";
             } else if (os.type() === "Windows") {
-                setSalsaEnvVariable = "setx VSCODE_TSJS 1";
+                setEnvironmentVariableCommand = "setx VSCODE_TSJS 1";
             }
 
-            let childProcess = new Node.ChildProcess();
-            childProcess.exec(setSalsaEnvVariable);
-
-            IntellisenseHelper.installTypescriptNext()
-                .then(function() {
-                    vscode.window.showInformationMessage("Salsa intellisense for VS Code was turned on. Restart to enable it.");
-                })
+            Q({})
+                .then(() => Q.ninvoke(child_process, "exec", setEnvironmentVariableCommand))
+                .then(() => IntellisenseHelper.installTypescriptNext())
+                .then(() => vscode.window.showInformationMessage("Salsa intellisense for VS Code was turned on. Restart to enable it."))
                 .done();
         }
     }
