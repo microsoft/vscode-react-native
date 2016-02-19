b8ecee4dcbd9ca22e597d3b9a60794d8cbb77925
digeff <digeff@hotmail.com>
2016-02-19 13:25:07 -0800

    Fix licenses

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 
 var child_process = require('child_process');
 var gulp = require('gulp');
