c5d308e47ff644d5ded8339fff0d80dc013275a8
digeff <digeff@hotmail.com>
2016-02-29 15:20:41 -0800

    Added public getter for extras variable in NestedError

--- src/common/nestedError.ts
+++ src/common/nestedError.ts
@@ -1,13 +1,17 @@
 export class NestedError extends Error {
-    private innerError: Error | any; // Normally this should be an error, but we support any value
-    private extras: any;
+    private _innerError: Error | any; // Normally this should be an error, but we support any value
+    private _extras: any;
 
     constructor(message: string, innerError: any, extras?: any) {
         super(message);
-        this.innerError = innerError;
+        this._innerError = innerError;
         this.name = innerError.name;
         const innerMessage = innerError.message;
         this.message = innerMessage ? `${message}: ${innerMessage}` : message;
-        this.extras = extras;
+        this._extras = extras;
+    }
+
+    public get extras(): any {
+        return this.extras;
     }
 }
