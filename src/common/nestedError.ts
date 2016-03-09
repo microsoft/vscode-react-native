77e8babb11f7d1d7c34f3b2164d88bc9cb485832
digeff <digeff@hotmail.com>
2016-03-09 08:48:22 -0800

    Fixed some files with missing copyrights

--- src/common/nestedError.ts
+++ src/common/nestedError.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 export class NestedError extends Error {
     private _innerError: Error | any; // Normally this should be an error, but we support any value
     private _extras: any;
