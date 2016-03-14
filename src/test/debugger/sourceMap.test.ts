6f664c64706a0084d724a8256958593fdd4cf433
Joshua Skelton <joskelto@microsoft.com>
2016-03-14 16:19:39 -0700

    Initial commit

--- src/test/debugger/sourceMap.test.ts
+++ src/test/debugger/sourceMap.test.ts
@@ -5,6 +5,7 @@ import {SourceMapUtil} from "../../debugger/sourceMap";
 
 import * as assert from "assert";
 import * as path from "path";
+import * as url from "url";
 
 suite("sourceMap", function() {
     suite("debuggerContext", function() {
@@ -13,9 +14,39 @@ suite("sourceMap", function() {
             const filePath = path.join("foo", "bar", "baz");
             const urlPath = "foo/bar/baz";
             const result = (<any>sourceMap).makeUnixStylePath(filePath);
-            assert(result === urlPath, `Expected "${urlPath}", found "${result}`);
+            assert(result === urlPath, `Expected "${urlPath}", found "${result}"`);
         });
 
-        // TODO: This class definitely needs more tests
+        test("should resolve a valid sourcemap url", function () {
+            const scriptUrl: url.Url = url.parse("http://localhost:8081/index.ios.bundle?platform=ios&dev=true");
+            const scriptBody = "//# sourceMappingURL=/index.ios.map?platform=ios&dev=true";
+            const expectedUrlHref = "http://localhost:8081/index.ios.map?platform=ios&dev=true";
+
+            const sourceMap = new SourceMapUtil();
+            const result = sourceMap.getSourceMapURL(scriptUrl, scriptBody);
+            assert.equal(expectedUrlHref, result.href);
+        });
+
+        test("should return null for an invalid sourcemap url", function () {
+            const scriptUrl: url.Url = url.parse("http://localhost:8081/index.ios.bundle?platform=ios&dev=true");
+            const scriptBody = "";
+
+            const sourceMap = new SourceMapUtil();
+            const result = sourceMap.getSourceMapURL(scriptUrl, scriptBody);
+            assert(result === null);
+        });
+
+        test("updateSourceMapFile", function() {
+            const sourceMapBody: string = "";
+            const scriptPath: string = "";
+            const sourcesRootPath: string = "";
+            const sourceMap = new SourceMapUtil();
+
+            const result: string = sourceMap.updateSourceMapFile(sourceMapBody, scriptPath, sourcesRootPath);
+        });
+
+        /*test("", function() {
+
+        });*/
     });
 });
