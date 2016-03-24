efebb488da00f43995a4c12e033a74091b0070c0
digeff <digeff@hotmail.com>
2016-03-24 13:40:56 -0700

    Created the ProcessExecution Recorder and Simulator to allow recording the events of a spanwed process, and then simulate them in a test

---
+++ src/test/resources/processExecution/recorder.ts
@@ -0,0 +1,133 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import * as child_process from "child_process";
+import * as events from "events";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+
+interface ISpawnOptions {
+    cwd?: string;
+    stdio?: any;
+    env?: any;
+    detached?: boolean;
+}
+
+import {ITimedEvent,
+    IEventArguments,
+    Recording,
+    IAndroidDevice,
+    IIOSDevice,
+    ISpawnArguments,
+} from "./recording";
+
+/* We use this class to capture the behavior of a ChildProces running inside of node, so we can store all the
+   visible events and side-effects of that process, and then we can perfectly reproduce them in a test by using
+   the Simulator class.
+
+   Call Recorder.installGlobalRecorder() when your program starts to record the events of all the spawned processes.
+   The recordings will be stored at the OS temporary directory e.g.:
+        Windows: %TEMP%\processExecutionRecording.txt
+        OS X: $TMPDIR/processExecutionRecording.txt
+*/
+export class Recorder {
+    private static originalSpawn: (command: string, args: string[], options: ISpawnOptions) => child_process.ChildProcess;
+
+    private recording: Recording;
+    private previousEventTimestamp: number;
+
+    public static installGlobalRecorder(): void {
+        if (!this.originalSpawn) {
+            this.originalSpawn = child_process.spawn;
+            child_process.spawn = this.recordAndSpawn.bind(this);
+        }
+    }
+
+    public static recordAndSpawn(command: string, args?: string[], options: ISpawnOptions = {}): child_process.ChildProcess {
+        const spawnedProcess = this.originalSpawn(command, args, options);
+        new Recorder(spawnedProcess, { command, args, options }).record();
+        return spawnedProcess;
+    }
+
+    constructor(private execution: child_process.ChildProcess, spawnArguments: ISpawnArguments,
+                private filePath = Recorder.defaultFilePath()) {
+        this.initializeRecording(spawnArguments);
+    }
+
+    public record(): void {
+        this.addListenerForRecordingEvent(this.execution.stdout, "stdout", "data", "data", data =>
+            data.toString());
+        this.addListenerForRecordingEvent(this.execution.stderr, "stderr", "data", "data", data =>
+            data.toString());
+        this.addListenerForRecordingEvent(this.execution, "error", "error", "error");
+        this.addListenerForRecordingEvent(this.execution, "exit", "exit", "code");
+        this.execution.on("error", () =>
+            this.store());
+        this.execution.on("exit", () =>
+            this.store());
+        this.previousEventTimestamp = this.now();
+    }
+
+    private initializeRecording(spawnArguments: ISpawnArguments): void {
+        /* The TBD values needs to be filled manually by the recorder, so we know the full context
+           where this recording was made */
+        this.recording = {
+            title: "TBD",
+            arguments: spawnArguments,
+            date: new Date(),
+            configuration: {
+                os: { platform: os.platform(), release: os.release() },
+                android: {
+                    sdk: { tools: "TBD", platformTools: "TBD", buildTools: "TBD", repositoryForSupportLibraries: "TBD" },
+                    intelHAXMEmulator: "TBD",
+                    visualStudioEmulator: "TBD",
+                },
+                reactNative: "TBD",
+                node: "TBD",
+                npm: "TBD",
+            },
+            state: {
+                reactNative: { packager: "TBD" },
+                devices: { android: <IAndroidDevice[]>[], ios: <IIOSDevice[]>[] },
+            },
+            events: <IEventArguments[]>[],
+        };
+    }
+
+    private static defaultFilePath(): string {
+        return path.join(os.tmpdir(), "processExecutionRecording.txt");
+    }
+
+    private now(): number {
+        return new Date().getTime();
+    }
+
+    private addListenerForRecordingEvent(emitter: events.EventEmitter, storedEventName: string, eventToListenName: string,
+                                         argumentName: string, argumentsConverter: (value: any) => any = value => value): void {
+        emitter.on(eventToListenName, (argument: any) => {
+            const now = this.now();
+            const relativeTimestamp = now - this.previousEventTimestamp;
+            this.previousEventTimestamp = now;
+            this.recording.events.push(this.generateEvent(relativeTimestamp, storedEventName, argumentName, argumentsConverter(argument)));
+        });
+    }
+
+    /* Generate an event based on the parameters e.g.: { "after": 0, "stdout": { "data": ":app:assembleDebug" } } */
+    private generateEvent(relativeTimestamp: number, eventName: string, argumentName: string, argument: any): IEventArguments {
+        const event: ITimedEvent = { after: relativeTimestamp };
+        (<any>event)[eventName] = this.generateEventArguments(argumentName, argument);
+        return <IEventArguments>event;
+    }
+
+    /* Generate the event arguments based on the parameters e.g.: { "data": ":app:assembleDebug" } */
+    private generateEventArguments(argumentName: string, argument: any): IEventArguments {
+        const eventArguments: IEventArguments = <IEventArguments>{};
+        (<any>eventArguments)[argumentName] = argument;
+        return eventArguments;
+    }
+
+    private store(): void {
+        fs.appendFileSync(this.filePath, JSON.stringify(this.recording) + "\n\n\n", "utf8");
+    }
+}
---
+++ src/test/resources/processExecution/recording.ts
@@ -0,0 +1,99 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+/* The recording is used by Recorder to store a recording, and by
+   Simulator to read it and simulate it.
+
+   File order: This file is ordered in a top-down approach */
+
+export interface Recording {
+    /* Metadata */
+    title: string;
+    arguments: ISpawnArguments;
+    date: Date;
+    configuration: MachineConfiguration;
+    state: MachineState;
+
+    /* Recorded events data */
+    events: IEventArguments[];
+}
+
+export interface ISpawnArguments {
+    command: string;
+    args: string[];
+    options: ISpawnOptions;
+}
+
+interface ISpawnOptions {
+    cwd?: string;
+    stdio?: any;
+    env?: any;
+    detached?: boolean;
+}
+
+export interface MachineConfiguration {
+    os: { platform: string, release: string };
+    android: {
+        sdk: { tools: string, platformTools: string, buildTools: string, repositoryForSupportLibraries: string };
+        intelHAXMEmulator: string;
+        visualStudioEmulator: string;
+    };
+    reactNative: string;
+    node: string;
+    npm: string;
+}
+
+export interface MachineState {
+    reactNative: { packager: PackagerStatus };
+    devices: { android: IAndroidDevice[], ios: IIOSDevice[] };
+}
+
+export type PackagerStatus = "Running" | "NotRunning" | "TBD";
+
+export interface IAndroidDevice {
+    id: string;
+    type: AndroidDeviceType;
+    hardware: string;
+    os: string;
+    api: number;
+    otherSpecs: string;
+    appStatus: AppStatusInDevice;
+}
+
+export type AndroidDeviceType = "SDKEmulator" | "VisualStudioEmulator" | "IntelHAXMEmulator_x86" | "IntelHAXMEmulator_x64" | "PhysicalDevice";
+
+export type AppStatusInDevice = "NotInstalled" | "Installed" | "Running" | "Debugging" | "TBD";
+
+export interface IIOSDevice {
+    id: string;
+    type: IIOSDeviceType;
+    appStatus: AppStatusInDevice;
+}
+
+export type IIOSDeviceType = "TBD";
+
+export type IEventArguments = IStdOutEvent | IStdErrEvent | IErrorEvent | IExitEvent | ICustomEvent;
+
+export interface IStdOutEvent extends ITimedEvent {
+    stdout: { data: string };
+}
+
+export interface IStdErrEvent extends ITimedEvent {
+    stderr: { data: string };
+}
+
+export interface IErrorEvent extends ITimedEvent {
+    error: { error: any };
+}
+
+export interface IExitEvent extends ITimedEvent {
+    exit: { code: number };
+}
+
+export interface ICustomEvent extends ITimedEvent {
+    custom: { lambda: () => Q.Promise<void> | void };
+}
+
+export interface ITimedEvent {
+    after: number;
+}
