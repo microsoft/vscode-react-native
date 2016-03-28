f98df3ce9c6ecb8d8e172dd43658b71af7ad3f9c
Meena Kunnathur Balakrishnan <meenap@microsoft.com>
2016-03-28 11:05:19 -0700

    Minor rewording of an error prompt

--- src/extension/rn-extension.ts
+++ src/extension/rn-extension.ts
@@ -103,7 +103,7 @@ function setupAndDispose<T extends ISetupableDisposable>(setuptableDisposable: T
 function warnWhenReactNativeVersionIsNotSupported(): void {
     return reactNativeProjectHelper.validateReactNativeVersion().done(() => { }, reason => {
         TelemetryHelper.sendSimpleEvent("unsupportedRNVersion", { rnVersion: reason });
-        const shortMessage = `React Native Tools only supports React Native versions 0.19.0 and later`;
+        const shortMessage = `React Native Tools extension only supports React Native versions 0.19.0 and later`;
         const longMessage = `${shortMessage}: ${reason}`;
         vscode.window.showWarningMessage(shortMessage);
         Log.logMessage(longMessage);
