d6e064a29e3af7b5293069f0fa1fd29d4c3a3eb8
digeff <digeff@hotmail.com>
2016-04-05 13:47:24 -0700

    Minor fix

--- src/common/interProcessMessageSender.ts
+++ src/common/interProcessMessageSender.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
+import * as assert from "assert";
+
 import * as net from "net";
 import * as Q from "q";
 import {Log} from "./log/log";
@@ -21,6 +23,7 @@ export interface IInterProcessMessageSender {
  */
 export class InterProcessMessageSender implements InterProcessMessageSender {
     constructor(private serverPath: string) {
+        assert(this.serverPath, "serverPath shouldn't be null");
     }
 
     public sendMessage(message: ExtensionMessage, args?: any[]): Q.Promise<any> {
@@ -28,7 +31,7 @@ export class InterProcessMessageSender implements InterProcessMessageSender {
         let messageWithArguments: MessageWithArguments = { message: message, args: args };
         let body = "";
 
-        let socket = net.connect(this.serverPath, function() {
+        let socket = net.connect(this.serverPath, () => {
             Log.logInternalMessage(LogLevel.Info, `Connected to socket at ${this.serverPath}`);
             let messageJson = JSON.stringify(messageWithArguments);
             socket.write(messageJson);
