e73ce04533f6c910a1bff033d72750a734fee007
Jimmy Thomson <jithomso@microsoft.com>
2016-04-11 16:13:36 -0700

    Fixing iOS launch when device is in debug mode
    When in debug mode the device does not produce any console output. We considered a launch successful when we saw the first message produced by the device, which would mean that the device launch would time out. With this change we consider the device started as soon as we send the final of the 3 messages to launch an app.

--- src/debugger/ios/deviceRunner.ts
+++ src/debugger/ios/deviceRunner.ts
@@ -20,7 +20,7 @@ export class DeviceRunner {
 
     public run(): Q.Promise<void> {
         const proxyPort = 9999;
-        const appLaunchStepTimeout = 5000;
+        const appLaunchStepTimeout = 10000;
         return new PlistBuddy().getBundleId(this.projectRoot, /*simulator=*/false)
             .then((bundleId: string) => this.getPathOnDevice(bundleId))
             .then((path: string) =>
@@ -49,7 +49,6 @@ export class DeviceRunner {
 
         const deferred1: Q.Deferred<net.Socket> = Q.defer<net.Socket>();
         const deferred2: Q.Deferred<net.Socket> = Q.defer<net.Socket>();
-        const deferred3: Q.Deferred<net.Socket> = Q.defer<net.Socket>();
 
         socket.on("data", (data: any): void => {
             data = data.toString();
@@ -76,16 +75,11 @@ export class DeviceRunner {
                     }
                 } else if (data[1] === "O") {
                     // STDOUT was written to, and the rest of the input until reaching a "#" is a hex-encoded string of that output
-                    if (initState === 3) {
-                        deferred3.resolve(socket);
-                        initState++;
-                    }
                 } else if (data[1] === "E") {
                     // An error has occurred, with error code given by data[2-3]: parseInt(data.substring(2, 4), 16)
                     const error = new Error("Unable to launch application.");
                     deferred1.reject(error);
                     deferred2.reject(error);
-                    deferred3.reject(error);
                 }
             }
         });
@@ -94,13 +88,11 @@ export class DeviceRunner {
             const error = new Error("Unable to launch application.");
             deferred1.reject(error);
             deferred2.reject(error);
-            deferred3.reject(error);
         });
 
         socket.on("error", function(err: Error): void {
             deferred1.reject(err);
             deferred2.reject(err);
-            deferred3.reject(err);
         });
 
         socket.connect(portNumber, "localhost", () => {
@@ -122,15 +114,12 @@ export class DeviceRunner {
                 deferred2.reject(new Error("Timeout launching application. Is the device locked?"));
             }, appLaunchStepTimeout);
             return deferred2.promise;
-        }).then((sock: net.Socket): Q.Promise<net.Socket> => {
+        }).then((sock: net.Socket): void => {
             // Continue execution; actually start the app running.
             const cmd: string = this.makeGdbCommand("c");
             initState++;
             sock.write(cmd);
-            setTimeout(function(): void {
-                deferred3.reject(new Error("Timeout launching application. Is the device locked?"));
-            }, appLaunchStepTimeout);
-            return deferred3.promise;
+            return;
         }).then(() => packagePath);
     }
 
