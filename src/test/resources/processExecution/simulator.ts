cc50e86a7806c9d4a81fce65304ae4b71bd3e9e8
Jimmy Thomson <jithomso@microsoft.com>
2016-04-12 10:44:21 -0700

    Updating test to account for new startup protocol

--- src/test/resources/processExecution/simulator.ts
+++ src/test/resources/processExecution/simulator.ts
@@ -76,7 +76,7 @@ export class Simulator {
 
         /* We call spawnWaitUntilFinished to fill the ISpawnResult object appropiatedly. The command
            and the arguments don't affect that object, so we just pass an empty command and parameters */
-        return new ChildProcess({ childProcess: fakeChildProcessModule }).spawnWaitUntilFinished("", []);
+        return new ChildProcess({ childProcess: fakeChildProcessModule }).spawn("", []);
     }
 
     public simulate(recording: Recording): Q.Promise<void> {
