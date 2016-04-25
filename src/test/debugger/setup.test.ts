6f8a5ead61f96e4c1f5886b7402808350ba9f491
digeff <digeff@hotmail.com>
2016-04-25 11:39:51 -0700

    Now we require.resolve the path to source map support instead of calculating it by hand. We also added a link to the bug

--- src/test/debugger/setup.test.ts
+++ src/test/debugger/setup.test.ts
@@ -4,11 +4,9 @@
 /* This setup is just to install the source-map-support, so we'll get TypeScript stack traces
    for the tests */
 import * as fs from "fs";
-import * as path from "path";
 
-// source-map-support has a bug that crashes some of our tests. We fix it
-const moduleRelativeLocation = "../../../node_modules/source-map-support/source-map-support.js";
-const moduleLocation = path.join(__dirname, moduleRelativeLocation);
+// source-map-support has a bug that crashes some of our tests. We fix it: https://github.com/evanw/node-source-map-support/issues/131
+const moduleLocation = require.resolve("source-map-support/source-map-support.js");
 const originalCode = "column -= 62;";
 const replacementCode = "if (column > 63) { column -= 62; }";
 const contents = fs.readFileSync(moduleLocation, "utf8");
