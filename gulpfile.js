92f1342210508a97151ac41d90fae5b7fcfcbfaf
Jimmy Thomson <jithomso@microsoft.com>
2016-08-08 16:29:40 -0700

    Adding gulp release

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ var ts = require('gulp-typescript');
 var mocha = require('gulp-mocha');
 var GulpExtras = require("./tools/gulp-extras");
 var minimist = require('minimist');
+var os = require("os");
+var fs = require("fs");
+var Q = require("q");
 
 var copyright = GulpExtras.checkCopyright;
 var imports = GulpExtras.checkImports;
@@ -133,7 +136,41 @@ gulp.task("clean", function () {
 });
 
 gulp.task("package", function (callback) {
-    var command = path.join(__dirname, "node_modules", ".bin", "vsce" + (process.platform === "win32" ? ".cmd" : ""));
+    var command = path.join(__dirname, "node_modules", ".bin", "vsce");
     var args = ["package"];
     executeCommand(command, args, callback);
 });
+
+gulp.task("release", ["build"], function () {
+    var licenseFiles = ["LICENSE.txt", "ThirdPartyNotices.txt"];
+    var backupFolder = path.resolve(path.join(os.tmpdir(), 'vscode-react-native'));
+    if (!fs.existsSync(backupFolder)) {
+        fs.mkdirSync(backupFolder);
+    }
+
+    return Q({})
+        .then(function () {
+            /* back up LICENSE.txt, ThirdPartyNotices.txt, README.md */
+            console.log("Backing up license files to " + backupFolder + "...");
+            licenseFiles.forEach(function (fileName) {
+                fs.writeFileSync(path.join(backupFolder, fileName), fs.readFileSync(fileName));
+            });
+
+            /* copy over the release package license files */
+            console.log("Preparing license files for release...");
+            fs.writeFileSync('LICENSE.txt', fs.readFileSync('release/LICENSE.txt'));
+            fs.writeFileSync('ThirdPartyNotices.txt', fs.readFileSync('release/ThirdPartyNotices.txt'));
+        }).then(()=>{
+            console.log("Creating release package...");
+            var deferred = Q.defer();
+            // NOTE: vsce must see npm 3.X otherwise it will not correctly strip out dev dependencies.
+            executeCommand('vsce', ['package'], function (arg) { if (arg) { deferred.reject(arg);} deferred.resolve()} , {cwd: path.resolve(__dirname)});
+            return deferred.promise;
+        }).finally(function () {
+            /* restore backed up files */
+            console.log("Restoring modified files...");
+            licenseFiles.forEach(function (fileName) {
+                fs.writeFileSync(path.join(__dirname, fileName), fs.readFileSync(path.join(backupFolder, fileName)));
+            });
+        });
+})
