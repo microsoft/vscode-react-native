9174feb78e9e98b72274e79fb2eef1605c48464e
Vladimir Kotikov <v-vlkoti@microsoft.com>
2017-02-06 16:41:42 +0300

    Dispose worker properly when reloading

--- src/debugger/appWorker.ts
+++ src/debugger/appWorker.ts
@@ -182,25 +182,25 @@ export class MultipleLifetimesAppWorker extends EventEmitter {
             Log.logMessage("Established a connection with the Proxy (Packager) to the React Native application"));
     }
 
-    private onMessage(message: string) {
-        function killWorker(worker: IDebuggeeWorker) {
-            if (!worker) return;
-            worker.stop();
-            worker = null;
-        }
+    private killWorker() {
+        if (!this.singleLifetimeWorker) return;
+        this.singleLifetimeWorker.stop();
+        this.singleLifetimeWorker = null;
+    }
 
+    private onMessage(message: string) {
         try {
             Log.logInternalMessage(LogLevel.Trace, "From RN APP: " + message);
             let object = <RNAppMessage>JSON.parse(message);
             if (object.method === "prepareJSRuntime") {
                 // In RN 0.40 Android runtime doesn't seem to be sending "$disconnected" event
                 // when user reloads an app, hence we need to try to kill it here either.
-                killWorker(this.singleLifetimeWorker);
+                this.killWorker();
                 // The MultipleLifetimesAppWorker will handle prepareJSRuntime aka create new lifetime
                 this.gotPrepareJSRuntime(object);
             } else if (object.method === "$disconnected") {
                 // We need to shutdown the current app worker, and create a new lifetime
-                killWorker(this.singleLifetimeWorker);
+                this.killWorker();
             } else if (object.method) {
                 // All the other messages are handled by the single lifetime worker
                 this.singleLifetimeWorker.postMessage(object);
