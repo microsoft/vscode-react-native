806dbd613c7164d672b334f1d942e525a6ae6054
Artem Egorov <v-aregor@microsoft.com>
2017-11-17 15:07:07 +0300

    Fix test

--- test/extension/android/androidPlatform.test.ts
+++ test/extension/android/androidPlatform.test.ts
@@ -178,7 +178,7 @@ suite("androidPlatform", function () {
                     }).then(() => {
                         should.assert(false, "runApp should've exited with an error");
                     }, reason => {
-                        reason.message.should.eql("Unknown error");
+                        reason.message.should.eql("Unknown error: not all success patterns were matched");
                     });
             });
 
