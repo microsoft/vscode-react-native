1d7f5fbc34a522f0e8f2c76b1ea143494f3f2c88
max-mironov <v-maximi@microsoft.com>
2018-01-31 11:09:24 +0300

    Added appcenter-node-client as an ext lib

---
+++ lib/app-center-node-client/models.d.ts
@@ -0,0 +1,20 @@
+/// <reference path="./src/account/models/index.d.ts" />
+/// <reference path="./src/codepush/models/index.d.ts" />
+/// <reference path="./src/analytics/models/index.d.ts" />
+/// <reference path="./src/distribute/models/index.d.ts" />
+/// <reference path="./src/build/models/index.d.ts" />
+/// <reference path="./src/crash/models/index.d.ts" />
+/// <reference path="./src/test/models/index.d.ts" />
+/// <reference path="./src/export/models/index.d.ts" />
+/// <reference path="./src/push/models/index.d.ts" />
+
+
+export * from './src/account/models/index';
+export * from './src/codepush/models/index';
+export * from './src/analytics/models/index';
+export * from './src/distribute/models/index';
+export * from './src/build/models/index';
+export * from './src/crash/models/index';
+export * from './src/test/models/index';
+export * from './src/export/models/index';
+export * from './src/push/models/index';
