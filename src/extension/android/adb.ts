40e26a369f1962720a21d51e892dabc4446b20af
Yuri Skorokhodov <yuri.skorokhodov@akvelon.com>
2018-12-06 14:30:20 +0300

    Add localize call for adb.ts

--- src/extension/android/adb.ts
+++ src/extension/android/adb.ts
@@ -9,6 +9,8 @@ import * as path from "path";
 import * as fs from "fs";
 import { ILogger } from "../log/LogHelper";
 import * as os from "os";
+import * as nls from "vscode-nls";
+const localize = nls.loadMessageBundle();
 
 // See android versions usage at: http://developer.android.com/about/dashboards/index.html
 export enum AndroidAPILevel {
@@ -141,7 +143,7 @@ export class AdbHelper {
         const matches = fileContent.match(/^sdk\.dir=(.+)$/m);
         if (!matches || !matches[1]) {
             if (logger) {
-                logger.info(`No sdk.dir value found in local.properties file. Using Android SDK location from PATH.`);
+                logger.info(localize("NoSdkDirFoundInLocalPropertiesFile", "No sdk.dir value found in local.properties file. Using Android SDK location from PATH."));
             }
             return null;
         }
@@ -152,7 +154,7 @@ export class AdbHelper {
             sdkLocation = sdkLocation.replace(/\\\\/g, "\\").replace("\\:", ":");
         }
         if (logger) {
-            logger.info(`Using Android SDK location defined in android/local.properties file: ${sdkLocation}.`);
+            logger.info(localize("UsindAndroidSDKLocationDefinedInLocalPropertiesFile", "Using Android SDK location defined in android/local.properties file: {0}.", sdkLocation));
         }
 
         return sdkLocation;
@@ -197,7 +199,7 @@ export class AdbHelper {
         const localPropertiesFilePath = path.join(projectRoot, "android", "local.properties");
         if (!fs.existsSync(localPropertiesFilePath)) {
             if (logger) {
-                logger.info(`local.properties file doesn't exist. Using Android SDK location from PATH.`);
+                logger.info(localize("LocalPropertiesFileDoesNotExist", "local.properties file doesn't exist. Using Android SDK location from PATH."));
             }
             return null;
         }
@@ -207,8 +209,8 @@ export class AdbHelper {
             fileContent = fs.readFileSync(localPropertiesFilePath).toString();
         } catch (e) {
             if (logger) {
-                logger.error(`Couldn't read from ${localPropertiesFilePath}.`, e, e.stack);
-                logger.info(`Using Android SDK location from PATH.`);
+                logger.error(localize("CouldNotReadFrom", "Couldn't read from {0}.", localPropertiesFilePath), e, e.stack);
+                logger.info(localize("UsingAndroidSDKLocationFromPATH", "Using Android SDK location from PATH."));
             }
             return null;
         }
