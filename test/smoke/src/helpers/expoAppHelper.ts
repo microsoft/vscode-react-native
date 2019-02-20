8f61096696ea47cc8dcc8ab56bf979543c3b43e6
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-02-20 14:59:34 +0300

    Add logging, fix comment

--- test/smoke/src/helpers/expoAppHelper.ts
+++ test/smoke/src/helpers/expoAppHelper.ts
@@ -23,13 +23,16 @@ export async function setupExpoAppAndroid(expoAppURL: string, expoAppAPKPath: st
         port: 4723,
         host: "localhost",
     };
-    // Expo application automatically detecting Expo URLs in the clipboard
+    console.log(`*** Copying ${expoAppURL} to system clipboard`);
+    // Expo application automatically detects Expo URLs in the clipboard
     // So we are copying expoAppURL to system clipboard and click on the special "Open from Clipboard" UI element
     clipboardy.writeSync(expoAppURL);
 
+    console.log(`*** Initializing WebdriverIO with options:/n ${opts}`);
     // Connect to the Android emulator with predefined opts
     const client = wdio.remote(opts);
 
+    console.log(`*** Running WebdriverIO...`);
     // Install application on emulator and run Expo app by expoAppURL
     await client.init()
     .waitForExist(EXPO_OPEN_FROM_CLIPBOARD, waitTime)
