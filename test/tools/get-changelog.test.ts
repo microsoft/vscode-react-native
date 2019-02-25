a046a44bbeb039312814efc43984d7a30615a9eb
Ruslan Bikkinin <v-rubikk@microsoft.com>
2019-02-25 17:17:37 +0300

    Add real changelog testcase

--- test/tools/get-changelog.test.ts
+++ test/tools/get-changelog.test.ts
@@ -5,6 +5,7 @@ import * as assert from "assert";
 import * as path from "path";
 import * as cp from "child_process";
 import * as Q from "q";
+import { writeFileSync } from "fs";
 
 const changelogsDir = path.join(__dirname, "resources", "changelogs");
 const changelogs = {
@@ -14,6 +15,7 @@ const changelogs = {
     severalVersions: path.join(changelogsDir, "several-versions.md"),
     emptyChangelogForVersion: path.join(changelogsDir, "empty-changelog-for-version.md"),
     includesReference: path.join(changelogsDir, "includes-reference.md"),
+    realChangelog: path.join(changelogsDir, "real-changelog.md"),
 };
 
 const scriptPath = path.join(__dirname, "..", "..", "tools", "get-changelog.js");
@@ -39,7 +41,13 @@ async function run(args: string[]) {
 
 async function check(args: string[], expectedOutput: string, codeIsZero: boolean) {
     const result = await run(args);
-    assert.equal(result.output.trim(), expectedOutput);
+    let actualOutput = result.output.trim();
+    // Consolidate newlines
+    actualOutput = actualOutput.replace(/\r\n/gm, "\n");
+    expectedOutput = expectedOutput.replace(/\r\n/gm, "\n");
+    writeFileSync("C:\\Users\\rusla\\out.txt", actualOutput);
+    writeFileSync("C:\\Users\\rusla\\out2.txt", expectedOutput);
+    assert.equal(actualOutput, expectedOutput);
     codeIsZero ? assert.equal(result.code, 0) : assert.notEqual(result.code, 0);
 }
 
@@ -65,7 +73,7 @@ suite("tools", () => {
         test("should throw error if specified version isn't mentioned in the file", async () => {
             await shouldFail([changelogs.oneVersion, "0.0.0"], "No version markers were found in the changelog file that would match specified version");
         });
-        test("should return version changelog if specified version is in the file and it is first version", async () => {
+        test("should return version changelog if specified version is in the file", async () => {
             await shouldSuccess([changelogs.oneVersion, "1.0.0"], "* some text1.0.0");
             await shouldSuccess([changelogs.severalVersions, "1.0.0"], "* some text1.0.0");
             await shouldSuccess([changelogs.severalVersions, "1.0.1"], "* some text1.0.1");
@@ -73,6 +81,30 @@ suite("tools", () => {
         });
         test("should throw error if changelog is empty for version", async () => {
             await shouldFail([changelogs.emptyChangelogForVersion, "1.0.1"], "No changelog content found or empty between 1.0.1 and previous version");
+        });
+        test("should return version changelog on real changelog", async () => {
+            const expected = `* Improved extension security
+* Improved error logging
+* Improved Expo login workflow
+* Added warning if RN application is being ran using \`Debug In Exponent\` configuration and \`expo\` package is not installed [#882](https://github.com/Microsoft/vscode-react-native/issues/882)
+* Fixed debugger url problem for haul projects [#875](https://github.com/Microsoft/vscode-react-native/issues/875)
+* Added localization for next languages:
+    * Chinese Simplified
+    * Chinese Traditional
+    * Japanese
+    * Korean
+    * German
+    * French
+    * Spanish
+    * Russian
+    * Italian
+    * Czech
+    * Turkish
+    * Portuguese
+    * Polish
+
+**Some localizations may not be completed, they we'll updated in future**`;
+            await shouldSuccess([changelogs.realChangelog, "0.8.0"], expected);
         });
     });
 });
