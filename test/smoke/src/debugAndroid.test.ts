aae8f95912ff48d0d217af436d35b65afbaae0e2
Ruslan Bikkinin <v-rubikk@microsoft.com>
2019-03-18 17:41:30 +0300

    Update test/smoke/src/debugAndroid.test.ts
    Co-Authored-By: SounD120 <yurapkr@yandex.ru>

--- test/smoke/src/debugAndroid.test.ts
+++ test/smoke/src/debugAndroid.test.ts
@@ -56,6 +56,7 @@ export function setup() {
             console.log("Android Debug test: Searching for \"Test output from debuggee\" string in console");
             let found = await app.workbench.debug.findStringInConsole("Test output from debuggee", 10000);
             assert.notStrictEqual(found, false, "\"Test output from debuggee\" string is missing in debug console");
+            console.log("Android Debug test: \"Test output from debuggee\" string is found");
             await app.workbench.debug.stopDebugging();
             console.log("Android Debug test: Debugging is stopped");
             client.closeApp();
