3492bb9dd22dad6deefc2d07436d061e4cf2fae2
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-03-26 15:12:39 +0300

    Fix appium stdio streams

--- test/smoke/src/helpers/appiumHelper.ts
+++ test/smoke/src/helpers/appiumHelper.ts
@@ -25,7 +25,7 @@ export class appiumHelper {
         const appiumLogPath = path.join(appiumLogFolder, "appium.log");
         console.log(`*** Executing Appium with logging to ${appiumLogPath}`);
         let appiumCommand = process.platform === "win32" ? "appium.cmd" : "appium";
-        appiumProcess = cp.spawn(appiumCommand, ["--log", appiumLogPath], {stdio:"inherit"});
+        appiumProcess = cp.spawn(appiumCommand, ["--log", appiumLogPath], {stdio:["inherit", "ignore", "inherit"]});
         appiumProcess.on("close", () => {
             console.log("*** Appium terminated");
         });
