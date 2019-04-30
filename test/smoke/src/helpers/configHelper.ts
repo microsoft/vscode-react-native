b82fd5ab24f9563e25b730439f92ebb994e9b0df
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-04-30 17:29:43 +0300

    Initial configurator for smoke tests

---
+++ test/smoke/src/helpers/configHelper.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+export class ConfigHelper {
+    public constructor(configPath: string) {
+
+    }
+}
