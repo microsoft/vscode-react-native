d49de66dd91e2dcb1815385c186d0aad64419427
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-05-08 13:32:12 +0300

    Add Expo debug iOS test for pure RN app

--- test/smoke/src/debugIos.test.ts
+++ test/smoke/src/debugIos.test.ts
@@ -5,7 +5,7 @@ import { SpectronApplication } from "./spectron/application";
 import * as assert from "assert";
 import { AppiumHelper, Platform, AppiumClient } from "./helpers/appiumHelper";
 import { SmokeTestsConstants } from "./helpers/smokeTestsConstants";
-import { RNworkspacePath, runVSCode, ExpoWorkspacePath } from "./main";
+import { RNworkspacePath, runVSCode, ExpoWorkspacePath, pureRNWorkspacePath } from "./main";
 import { IosSimulatorHelper } from "./helpers/iosSimulatorHelper";
 import { sleep, findFile } from "./helpers/utilities";
 import { SetupEnvironmentHelper } from "./helpers/setupEnvironmentHelper";
@@ -137,6 +137,71 @@ export function setup() {
             console.log("iOS Expo Debug test: \"Test output from debuggee\" string is found");
             await app.workbench.debug.stopDebugging();
             console.log("iOS Expo Debug test: Debugging is stopped");
+        });
+
+        it("Pure RN app Expo test", async function () {
+            this.timeout(debugExpoTestTime);
+            app = await runVSCode(pureRNWorkspacePath);
+            console.log(`iOS pure RN Expo test: ${pureRNWorkspacePath} directory is opened in VS Code`);
+            await app.workbench.explorer.openExplorerView();
+            await app.workbench.explorer.openFile("App.js");
+            await app.runCommand("cursorTop");
+            console.log("iOS pure RN Expo test: App.js file is opened");
+            await app.workbench.debug.setBreakpointOnLine(23);
+            console.log("iOS pure RN Expo test: Breakpoint is set on line 23");
+            await app.workbench.debug.openDebugViewlet();
+            console.log(`iOS pure RN Expo test: Chosen debug configuration: ${ExpoDebugConfigName}`);
+            await app.workbench.debug.chooseDebugConfiguration(ExpoDebugConfigName);
+            // We need to implicitly add target to "Debug iOS" configuration to avoid running additional simulator
+            SetupEnvironmentHelper.addIosTargetToLaunchJson(RNworkspacePath);
+            console.log("iOS pure RN Expo test: Starting debugging");
+            await app.workbench.debug.startDebugging();
+            const device = <string>IosSimulatorHelper.getDevice();
+            await sleep(5 * 1000);
+            await app.workbench.waitForTab("Expo QR Code");
+            await app.workbench.waitForActiveTab("Expo QR Code");
+            console.log("iOS pure RN Expo test: 'Expo QR Code' tab found");
+            await app.workbench.selectTab("Expo QR Code");
+            console.log("iOS pure RN Expo test: 'Expo QR Code' tab selected");
+            let expoURL;
+            for (let retries = 0; retries < 5; retries++) {
+                await app.workbench.selectTab("Expo QR Code");
+                expoURL = await app.workbench.debug.prepareExpoURLToClipboard();
+                if (expoURL) break;
+            }
+            assert.notStrictEqual(expoURL, null, "Expo URL pattern is not found in the clipboard");
+            expoURL = expoURL as string;
+            let appFile = findFile(SetupEnvironmentHelper.iOSExpoAppsCacheDir, /.*\.(app)/);
+            if (!appFile) {
+                throw new Error(`iOS Expo app is not found in ${SetupEnvironmentHelper.iOSExpoAppsCacheDir}`);
+            }
+            const appPath = path.join(SetupEnvironmentHelper.iOSExpoAppsCacheDir, appFile);
+            const opts = AppiumHelper.prepareAttachOptsForIosApp(device, appPath);
+            let client = AppiumHelper.webdriverAttach(opts);
+            clientInited = client.init();
+            await AppiumHelper.openExpoApplication(Platform.iOS, clientInited, expoURL);
+            console.log(`iOS pure RN Expo test: Waiting ${SmokeTestsConstants.expoAppBuildAndInstallTimeout}ms until Expo app is ready...`);
+            await sleep(SmokeTestsConstants.expoAppBuildAndInstallTimeout);
+            // Sometimes by this moment iOS app already have remote js debugging enabled so we don't need to enable it manually
+            if (!await app.workbench.debug.areStackFramesExist()) {
+                await AppiumHelper.disableExpoErrorRedBox(clientInited);
+                await AppiumHelper.enableRemoteDebugJS(clientInited, Platform.iOS_Expo);
+                await sleep(5 * 1000);
+            }
+            await app.workbench.debug.waitForDebuggingToStart();
+            console.log("iOS pure RN Expo test: Debugging started");
+            await app.workbench.debug.waitForStackFrame(sf => sf.name === "App.js" && sf.lineNumber === 23, "looking for App.js and line 23");
+            console.log("iOS pure RN Expo test: Stack frame found");
+            await app.workbench.debug.continue();
+            await app.workbench.debug.continue();
+            // Wait for our debug string to render in debug console
+            await sleep(500);
+            console.log("iOS pure RN Expo test: Searching for \"Test output from debuggee\" string in console");
+            let found = await app.workbench.debug.findStringInConsole("Test output from debuggee", 10000);
+            assert.notStrictEqual(found, false, "\"Test output from debuggee\" string is missing in debug console");
+            console.log("iOS pure RN Expo test: \"Test output from debuggee\" string is found");
+            await app.workbench.debug.stopDebugging();
+            console.log("iOS pure RN Expo test: Debugging is stopped");
         });
     });
 }
