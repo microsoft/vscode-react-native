8f1c6bce84dead9228dbcc81cb81e0937b330888
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-05-15 16:48:38 +0300

    Refactor configurator workflow with local files

--- test/smoke/src/helpers/configHelper.ts
+++ test/smoke/src/helpers/configHelper.ts
@@ -10,10 +10,14 @@ export interface TestRunArguments {
 }
 
 export interface TestEnvVariables {
-    ANDROID_EMULATOR: string;
-    IOS_SIMULATOR: string;
-    ANDROID_VERSION: string;
-    IOS_VERSION: string;
+    android: {
+        ANDROID_EMULATOR: string;
+        ANDROID_VERSION: string;
+    }
+    ios?: {
+        IOS_SIMULATOR: string;
+        IOS_VERSION: string;
+    }
     CODE_VERSION: string;
 }
 
@@ -21,21 +25,17 @@ export class TestConfigurator {
     // Read json file with env variables for the test
     public static readTestEnvVariables(configFilePath: string) {
         const config = this.parseConfigFile(configFilePath);
-        if (config.ANDROID_EMULATOR && !process.env.ANDROID_EMULATOR) {
-            process.env.ANDROID_EMULATOR = config.ANDROID_EMULATOR;
-        }
-        if (config.IOS_SIMULATOR && !process.env.IOS_SIMULATOR) {
-            process.env.IOS_SIMULATOR = config.IOS_SIMULATOR;
-        }
-        if (config.ANDROID_VERSION && !process.env.ANDROID_VERSION) {
-            process.env.ANDROID_VERSION = config.ANDROID_VERSION;
-        }
-        if (config.IOS_VERSION && !process.env.IOS_VERSION) {
-            process.env.IOS_VERSION = config.IOS_VERSION;
-        }
-        if (config.CODE_VERSION && !process.env.CODE_VERSION) {
+        if (config) {
+            if (config.ios) {
+                process.env.IOS_SIMULATOR = config.ios.IOS_SIMULATOR;
+                process.env.IOS_VERSION = config.ios.IOS_VERSION;
+            }
+            process.env.ANDROID_EMULATOR = config.android.ANDROID_EMULATOR;
+            process.env.ANDROID_VERSION = config.android.ANDROID_VERSION;
             process.env.CODE_VERSION = config.CODE_VERSION;
         }
+
+        // If config file is absent - use standard env variables if defined
         this.printEnvVariableConfiguration();
     }
 
@@ -57,18 +57,46 @@ export class TestConfigurator {
         console.log(initLog);
     }
 
-    private static parseConfigFile(configFilePath: string): TestEnvVariables {
+    private static parseConfigFile(configFilePath: string): TestEnvVariables | null {
         if (fs.existsSync(configFilePath)) {
             const config = JSON.parse(fs.readFileSync(configFilePath).toString());
+
+            let android;
+            if (config.ANDROID_EMULATOR && config.ANDROID_VERSION) {
+                android = {
+                    ANDROID_EMULATOR: config.ANDROID_EMULATOR,
+                    ANDROID_VERSION: config.ANDROID_VERSION,
+                }
+            } else {
+                throw "Incorrect config: absent Android fields"
+            }
+
+            let ios;
+            if (process.platform === "darwin") {
+                if (config.IOS_SIMULATOR && config.IOS_VERSION) {
+                    ios = {
+                        IOS_SIMULATOR: config.IOS_SIMULATOR,
+                        IOS_VERSION: config.IOS_VERSION,
+                    }
+                } else {
+                    throw "Incorrect config: absent iOS fields"
+                }
+            }
+
+            let CODE_VERSION;
+            if (config.CODE_VERSION) {
+                CODE_VERSION = config.CODE_VERSION;
+            } else {
+                throw "Incorrect config: absent VS Code version field"
+            }
+
             return {
-                ANDROID_EMULATOR: config.ANDROID_EMULATOR,
-                IOS_SIMULATOR: config.IOS_SIMULATOR,
-                ANDROID_VERSION: config.ANDROID_VERSION,
-                IOS_VERSION: config.IOS_VERSION,
-                CODE_VERSION: config.CODE_VERSION,
+                android: android,
+                ios: ios,
+                CODE_VERSION: CODE_VERSION,
             };
         } else {
-            throw new Error("Config file is not found");
+            return null;
         }
     }
 }
