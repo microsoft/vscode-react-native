b2fe8af0f73cda499d0a6a38a946946d57458043
Yuri Skorokhodov <yurapkr@yandex.ru>
2019-05-27 16:18:34 +0300

    Update test/smoke/src/main.ts
    Co-Authored-By: Ruslan Bikkinin <v-rubikk@microsoft.com>

--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -253,7 +253,7 @@ describe("Extension smoke tests", () => {
         }
     } else {
         if (testParams.RunBasicTests) {
-            console.log("*** --basic-only parameter is set, Android native tests will be ran");
+            console.log("*** --basic-only parameter is set, basic Android tests will be ran");
             setupReactNativeDebugAndroidTests(testParams);
         } else {
             setupReactNativeDebugAndroidTests();
