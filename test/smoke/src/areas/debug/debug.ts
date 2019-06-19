6fa7fc2c028eeb505b9d607d4b1ff260439e171f
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-06-19 11:29:26 +0300

    Fix doubleclick function

--- test/smoke/src/areas/debug/debug.ts
+++ test/smoke/src/areas/debug/debug.ts
@@ -143,9 +143,7 @@ export class Debug extends Viewlet {
     }
 
     public async focusDebugConsole() {
-        await this.spectron.client.waitAndClick(DEBUG_CONSOLE_AREA);
-        await sleep(300);
-        await this.spectron.client.waitAndClick(DEBUG_CONSOLE_AREA);
+        await this.spectron.client.doubleClickAndWait(DEBUG_CONSOLE_AREA);
         await sleep(300);
     }
 
