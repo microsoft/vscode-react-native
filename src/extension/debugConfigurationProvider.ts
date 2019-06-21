a61cb9abf821c0401294901fc76f432fe1f3584d
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-06-21 16:48:36 +0300

    Fix indentations

--- src/extension/debugConfigurationProvider.ts
+++ src/extension/debugConfigurationProvider.ts
@@ -6,41 +6,41 @@ import * as vscode from "vscode";
 export class ReactNativeDebugConfigProvider implements vscode.DebugConfigurationProvider {
 
     public configurations = {
-    "Debug Android": {
-        "name": "Debug Android",
-        "program": "${workspaceRoot}/.vscode/launchReactNative.js",
-        "type": "reactnative",
-        "request": "launch",
-        "platform": "android",
-        "sourceMaps": true,
-        "outDir": "${workspaceRoot}/.vscode/.react",
-    },
-    "Debug iOS": {
-        "name": "Debug iOS",
-        "program": "${workspaceRoot}/.vscode/launchReactNative.js",
-        "type": "reactnative",
-        "request": "launch",
-        "platform": "ios",
-        "sourceMaps": true,
-        "outDir": "${workspaceRoot}/.vscode/.react",
-    },
-    "Attach to packager": {
-        "name": "Attach to packager",
-        "program": "${workspaceRoot}/.vscode/launchReactNative.js",
-        "type": "reactnative",
-        "request": "attach",
-        "sourceMaps": true,
-        "outDir": "${workspaceRoot}/.vscode/.react",
-    },
-    "Debug in Exponent": {
-        "name": "Debug in Exponent",
-        "program": "${workspaceRoot}/.vscode/launchReactNative.js",
-        "type": "reactnative",
-        "request": "launch",
-        "platform": "exponent",
-        "sourceMaps": true,
-        "outDir": "${workspaceRoot}/.vscode/.react",
-    }};
+        "Debug Android": {
+            "name": "Debug Android",
+            "program": "${workspaceRoot}/.vscode/launchReactNative.js",
+            "type": "reactnative",
+            "request": "launch",
+            "platform": "android",
+            "sourceMaps": true,
+            "outDir": "${workspaceRoot}/.vscode/.react",
+        },
+        "Debug iOS": {
+            "name": "Debug iOS",
+            "program": "${workspaceRoot}/.vscode/launchReactNative.js",
+            "type": "reactnative",
+            "request": "launch",
+            "platform": "ios",
+            "sourceMaps": true,
+            "outDir": "${workspaceRoot}/.vscode/.react",
+        },
+        "Attach to packager": {
+            "name": "Attach to packager",
+            "program": "${workspaceRoot}/.vscode/launchReactNative.js",
+            "type": "reactnative",
+            "request": "attach",
+            "sourceMaps": true,
+            "outDir": "${workspaceRoot}/.vscode/.react",
+        },
+        "Debug in Exponent": {
+            "name": "Debug in Exponent",
+            "program": "${workspaceRoot}/.vscode/launchReactNative.js",
+            "type": "reactnative",
+            "request": "launch",
+            "platform": "exponent",
+            "sourceMaps": true,
+            "outDir": "${workspaceRoot}/.vscode/.react",
+        }};
 
     public async provideDebugConfigurations(folder: vscode.WorkspaceFolder | undefined, token?: vscode.CancellationToken): Promise<vscode.DebugConfiguration[]> {
         const pickedConfigs = await vscode.window.showQuickPick(["Debug Android", "Debug iOS", "Attach to packager", "Debug in Exponent"], {canPickMany: true}, token);
