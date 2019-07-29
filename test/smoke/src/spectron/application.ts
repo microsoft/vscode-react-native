960dbab3f8513f111a44abdded630551e64edf41
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-07-29 11:49:00 +0300

    Revert "Bump spectron version"
    This reverts commit c4918de21fed38ad2e01d9e0ea45846ae44ce6a4.

--- test/smoke/src/spectron/application.ts
+++ test/smoke/src/spectron/application.ts
@@ -222,12 +222,6 @@ export class SpectronApplication {
         const extensionLogsDir = path.join(artifactsPath, runName, "extensionLogs");
         const userDataDir = path.join(this.options.userDataDir, runName);
         chromeDriverArgs.push(`--user-data-dir=${userDataDir}`);
-        if (process.platform === "win32") {
-            // Chrome throws an error "Lost UI Context" on Windows
-            // This arg forces Chrome to use SwiftShader rendering to avoid errors during headless testing
-            // https://bugs.chromium.org/p/chromium/issues/detail?id=729961
-            chromeDriverArgs.push("--use-gl=swiftshader");
-        }
 
         const env = {
             path: process.env.path || process.env.PATH,
