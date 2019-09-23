6ed02339bb199027f88eba7ea6225699ed1b5220
RedMickey <mihax2013@yandex.ru>
2019-09-23 12:27:59 +0300

    Add HermesReactNativeSample project 2

---
+++ test/smoke/resources/HermesReactNativeSample/App.js
@@ -0,0 +1,89 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import TestButton from './TestButton';
+
+  const isHermes = () => global.HermesInternal != null;
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <TestButton />
+          <View>
+            <Text style={{color: 'red'}}>
+              {`Hermes is working: ${isHermes().toString()}`}
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ test/smoke/resources/HermesReactNativeSample/TestButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Button } from 'react-native';
+
+export default class TestButton extends Component {
+    constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      let testBoleanValue = true;
+      console.log('Test output from debuggee');
+    }
+    render() {
+      return (
+      <View style={{marginTop: 10}}>
+          <Button
+            onPress={this.handleClick}
+            title="Test Button"
+          />
+      </View>
+      );
+    }
+  }
+
+
