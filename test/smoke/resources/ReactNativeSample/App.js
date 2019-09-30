fe2fc6b7c3f704492e4e3daa127b534a9fa9a5e5
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-09-30 13:15:14 +0300

    Fix lines in sample app.js file

--- test/smoke/resources/ReactNativeSample/App.js
+++ test/smoke/resources/ReactNativeSample/App.js
@@ -5,6 +5,7 @@
  * @format
  * @flow
  */
+
 import React from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
