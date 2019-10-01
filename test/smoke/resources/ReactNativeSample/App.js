3dd908dc8a94a486f0e98dc1839c0eed1f1d8e8f
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-10-01 11:04:15 +0300

    Apply suggestions from code review

--- test/smoke/resources/ReactNativeSample/App.js
+++ test/smoke/resources/ReactNativeSample/App.js
@@ -11,7 +11,6 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 
-
 const App: () => React$Node = () => {
   return (
     <>
