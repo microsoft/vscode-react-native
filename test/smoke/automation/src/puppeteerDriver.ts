1e0c9f649ebd36ed14b6cfa0c435c6519cb6e09b
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-11-11 13:12:38 +0300

    Change copyrights style

--- test/smoke/automation/src/puppeteerDriver.ts
+++ test/smoke/automation/src/puppeteerDriver.ts
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import * as puppeteer from "puppeteer";
 import { ChildProcess, spawn } from "child_process";
--- test/smoke/automation/tools/copy-package-version.js
+++ test/smoke/automation/tools/copy-package-version.js
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 
 const fs = require('fs');
 const path = require('path');
