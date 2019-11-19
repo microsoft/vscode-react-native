3f0f1050cab48687897a3030da9ad34649382b6c
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-11-19 13:35:29 +0300

    Refactor code

--- src/debugger/appWorker.ts
+++ src/debugger/appWorker.ts
@@ -224,10 +224,9 @@ function fetch(url) {
         this.DOM_LOCATION_STUB = `// https://github.com/callstack/haul/blob/7cd75dfc0775c5a31a32b2965883ed174cee6aef/packages/haul-core/assets/debuggerWorker.js#L123
 // Since Haul 0.14.2 version it relies on DOM location object presence in global object
 // but since worker is ran as a Node process it doesn't have a location, so we add it
-if (self.location) {
-    return;
+if (!self.location) {
+    self.location = new URL('http://${this.packagerAddress}:${this.packagerPort}');
 }
-self.location = new URL('http://${this.packagerAddress}:${this.packagerPort}');
 `;
         this.scriptImporter = new ScriptImporter(this.packagerAddress, this.packagerPort, sourcesStoragePath, this.packagerRemoteRoot, this.packagerLocalRoot);
     }
