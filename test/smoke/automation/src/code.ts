514d1c610b663ff80c4ac3197138191b787a002a
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-12-03 13:29:55 +0300

    Revert "Resolve problems with smoke tests driver handle"
    This reverts commit f1a4f088f127f1047ed73b694df79480fda9b511.

--- test/smoke/automation/src/code.ts
+++ test/smoke/automation/src/code.ts
@@ -6,6 +6,7 @@ import * as cp from "child_process";
 import * as os from "os";
 import * as fs from "fs";
 import * as mkdirp from "mkdirp";
+import { tmpName } from "tmp";
 import { IDriver, connect as connectElectronDriver, IDisposable, IElement, Thenable } from "./driver";
 import { connect as connectPuppeteerDriver, launch } from "./puppeteerDriver";
 import { Logger } from "./logger";
@@ -102,15 +103,12 @@ export interface SpawnOptions {
     headless?: boolean;
 }
 
-async function createDriverHandle(userDataDir?: string): Promise<string> {
+async function createDriverHandle(): Promise<string> {
     if ("win32" === os.platform()) {
         const name = [...Array(15)].map(() => Math.random().toString(36)[3]).join("");
         return `\\\\.\\pipe\\${name}`;
     } else {
-        const driverHandlePath = path.join(userDataDir!, "SmokeTestDriverHandle");
-        mkdirp.sync(userDataDir!);
-        cp.execSync(`mkfifo ${driverHandlePath}`);
-        return driverHandlePath;
+        return tmpName();
     }
 }
 
