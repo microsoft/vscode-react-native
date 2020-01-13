b68f492443b79f1dfc05c3603de26c63480ac6e8
David Serafimov <daseramo@microsoft.com>
2020-01-13 11:52:26 -0800

    Handle react-native-windows not installed as an exception.
    Remove react-native-windows warning and instead throw an exception when it seems that react-native-windows is not installed.

--- src/common/error/errorStrings.ts
+++ src/common/error/errorStrings.ts
@@ -42,6 +42,7 @@ export const ERROR_STRINGS = {
     [InternalErrorCode.CouldNotFindLocationOfNodeDebugger]: localize("CouldNotFindLocationOfNodeDebugger", "Couldn't find the location of the node-debugger extension"),
     [InternalErrorCode.CouldNotFindWorkspace]: localize("CouldNotFindWorkspace", "Couldn't find any workspace or React Native project folder"),
     [InternalErrorCode.ReactNativePackageIsNotInstalled]: localize("ReactNativePackageIsNotInstalled", "Couldn't find react-native package in node_modules. Please, run \"npm install\" inside your project to install it."),
+    [InternalErrorCode.ReactNativeWindowsIsNotInstalled]: localize("ReactNativeWindowsIsNotInstalled", "It appears you don't have react-native-windows installed. Go to https://github.com/microsoft/react-native-windows#getting-started for more info."),
     [InternalErrorCode.PackagerRunningInDifferentPort]: localize("PackagerRunningInDifferentPort", "A packager cannot be started on port {0} because a packager process is already running on port {1}"),
     [InternalErrorCode.ErrorWhileProcessingMessageInIPMSServer]: localize("ErrorWhileProcessingMessageInIPMSServer", "An error ocurred while handling message: {0}"),
     [InternalErrorCode.ErrorNoPipeFound]: localize("ErrorNoPipeFound", "Unable to set up communication with VSCode react-native extension. Is this a react-native project, and have you made sure that the react-native npm package is installed at the root?"),
