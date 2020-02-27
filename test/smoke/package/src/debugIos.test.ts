598784864fd63eefd368cb6044d2afc63950dd2d
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-02-27 12:14:46 +0300

    Fix tslint

--- test/smoke/package/src/debugIos.test.ts
+++ test/smoke/package/src/debugIos.test.ts
@@ -68,8 +68,7 @@ export function setup(testParameters?: TestRunArguments) {
                             console.log(`Attempt to start #${retry} failed, retrying...`);
                         }
                     }
-                }
-                else {
+                } else {
                     assert.fail("REACT_NATIVE_TOOLS_LOGS_DIR is not defined");
                 }
             }
