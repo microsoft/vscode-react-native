78bdc57512edd4ae0760d738dc1193e30dc4b659
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-03-05 11:20:31 +0300

    Add expo tests for different Expo network scenarios (#1208)
    * Refactor Expo tests

--- test/smoke/automation/src/quickopen.ts
+++ test/smoke/automation/src/quickopen.ts
@@ -90,6 +90,16 @@ export class QuickOpen {
         await this.code.waitAndClick(QuickOpen.QUICK_OPEN_FOCUSED_ELEMENT);
     }
 
+    public async runDebugScenario(scenario: string): Promise<void> {
+        await this.openQuickOpen(`debug ${scenario}`);
+
+        // wait for the best choice to be focused
+        await this.code.waitForTextContent(QuickOpen.QUICK_OPEN_FOCUSED_ELEMENT, scenario);
+
+        // wait and click on the best choice
+        await this.code.waitAndClick(QuickOpen.QUICK_OPEN_FOCUSED_ELEMENT);
+    }
+
     public async openQuickOutline(): Promise<void> {
         let retries = 0;
 
