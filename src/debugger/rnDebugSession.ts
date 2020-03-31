39f72c059bde96f8f2fe42982238b19325d63a13
RedMickey <mihax2013@yandex.ru>
2020-03-31 09:41:35 +0300

    Add description for rnDebugSessionId parameter

--- src/debugger/rnDebugSession.ts
+++ src/debugger/rnDebugSession.ts
@@ -219,6 +219,9 @@ export class RNDebugSession extends LoggingDebugSession {
                 continueOnAttach: true,
                 port: this.cdpProxyPort,
                 smartStep: false,
+                // The unique identifier of the debug session. It is used to distinguish React Native extension's
+                // debug sessions from other ones. So we can save and process only the extension's debug sessions
+                // in vscode.debug API methods "onDidStartDebugSession" and "onDidTerminateDebugSession".
                 rnDebugSessionId: this.session.id,
             };
 
