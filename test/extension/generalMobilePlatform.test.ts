b747cf3c6f9045030e0a49b2cfc06a7d7a535499
JiglioNero <43934531+JiglioNero@users.noreply.github.com>
2020-04-06 12:47:50 +0300

    Update test/extension/generalMobilePlatform.test.ts
    Co-Authored-By: RedMickey <33267199+RedMickey@users.noreply.github.com>

--- test/extension/generalMobilePlatform.test.ts
+++ test/extension/generalMobilePlatform.test.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-
 import * as assert from "assert";
 import { GeneralMobilePlatform } from "../../src/extension/generalMobilePlatform";
 import * as fs from "fs";
