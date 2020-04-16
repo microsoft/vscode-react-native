196d5999a1a97449d63bd3dd06ab8b879733c098
JiglioNero <smouk.chayz@gmail.com>
2020-04-16 15:35:14 +0300

    start migrate

---
+++ test/runTest.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import * as path from "path";
+
+import { runTests } from "vscode-test";
+
+async function goTest() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, "..");
+
+    // The path to the extension test runner script
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, "index");
+    console.log(extensionTestsPath);
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+  } catch (err) {
+    console.error(err);
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
+}
+
+goTest();
