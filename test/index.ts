fc54485bc1f90e90e15ad821d47d340d12bf4ae4
RedMickey <mihax2013@yandex.ru>
2020-04-17 13:47:58 +0300

    Fix code style

--- test/index.ts
+++ test/index.ts
@@ -27,28 +27,25 @@ export function run(): Promise<void> {
     // Register Mocha options
     return new Promise((resolve, reject) => {
         glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-          if (err) {
-            return reject(err);
-          }
+            if (err) {
+                return reject(err);
+            }
 
-          // Add files to the test suite
-          files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-          try {
-            // Run the mocha test
-            mocha.run((failures: any) => {
-              if (failures > 0) {
-                reject(new Error(`${failures} tests failed.`));
-              } else {
-                resolve();
-              }
-            });
-          } catch (err) {
-            reject(err);
-          }
+            try {
+                // Run the mocha test
+                mocha.run((failures: any) => {
+                    if (failures > 0) {
+                        reject(new Error(`${failures} tests failed.`));
+                    } else {
+                        resolve();
+                    }
+                });
+            } catch (err) {
+                reject(err);
+            }
         });
-      });
+    });
 }
-
-
-
