3c461d539a1bb278cbaf6d4c42da3889ac78b379
Mikhail Suendukov <43934531+JiglioNero@users.noreply.github.com>
2020-07-16 13:18:44 +0300

    Revert "remove restart packager button"
    This reverts commit adefe5ad69ad1a720fd7ea360f606eff6aef2614.

--- src/extension/packagerStatusIndicator.ts
+++ src/extension/packagerStatusIndicator.ts
@@ -18,49 +18,86 @@ export enum PackagerStatus {
 }
 
 export class PackagerStatusIndicator implements Disposable {
+    private packagerStatusItem: StatusBarItem;
     private togglePackagerItem: StatusBarItem;
+    private restartPackagerItem: StatusBarItem;
     private static PACKAGER_NAME: string = localize("ReactNativePackager", "React Native Packager");
+    private static PACKAGER_STATUS_STOPPED: string = localize("PackagerStopped", "{0}: Stopped", PackagerStatusIndicator.PACKAGER_NAME);
+    private static PACKAGER_STATUS_STOPPING: string = localize("PackagerStopping", "{0}: Stopping", PackagerStatusIndicator.PACKAGER_NAME);
+    private static PACKAGER_STATUS_STARTED: string = localize("PackagerStarted", "{0}: Started", PackagerStatusIndicator.PACKAGER_NAME);
+    private static PACKAGER_STATUS_STARTING: string = localize("PackagerStarting", "{0}: Starting", PackagerStatusIndicator.PACKAGER_NAME);
 
     private static START_ICON = "$(triangle-right)";
     private static STOP_ICON = "$(primitive-square)";
-    private static ACTIVITY_ICON = "$(tree-item-loading)";
+    private static RESTART_ICON = "$(sync)";
+    private static ACTIVITY_ICON = "$(watch)";
 
     private static START_COMMAND = "reactNative.startPackager-preview";
     private static STOP_COMMAND = "reactNative.stopPackager-preview";
+    private static RESTART_COMMAND = "reactNative.restartPackager-preview";
 
     public constructor() {
+        this.packagerStatusItem = window.createStatusBarItem(StatusBarAlignment.Left, 10);
+        this.packagerStatusItem.text = PackagerStatusIndicator.PACKAGER_STATUS_STOPPED;
+        this.packagerStatusItem.show();
+
         this.togglePackagerItem = window.createStatusBarItem(StatusBarAlignment.Left, 10);
-        this.togglePackagerItem.text = `${PackagerStatusIndicator.START_ICON} ${PackagerStatusIndicator.PACKAGER_NAME}`;
+        this.togglePackagerItem.text = PackagerStatusIndicator.START_ICON;
         this.togglePackagerItem.command = PackagerStatusIndicator.START_COMMAND;
         this.togglePackagerItem.tooltip = localize("StartPackager", "Start Packager");
         this.togglePackagerItem.show();
+
+        this.restartPackagerItem = window.createStatusBarItem(StatusBarAlignment.Left, 10);
+        this.restartPackagerItem.text = PackagerStatusIndicator.RESTART_ICON;
+        this.restartPackagerItem.command = PackagerStatusIndicator.RESTART_COMMAND;
+        this.restartPackagerItem.tooltip = localize("RestartPackager", "Restart Packager");
+        this.restartPackagerItem.show();
+
     }
 
     public dispose(): void {
+        this.packagerStatusItem.dispose();
         this.togglePackagerItem.dispose();
+        this.restartPackagerItem.dispose();
     }
 
     public updatePackagerStatus(status: PackagerStatus): void {
         switch (status) {
             case PackagerStatus.PACKAGER_STOPPED:
-                this.togglePackagerItem.text =  `${PackagerStatusIndicator.START_ICON} ${PackagerStatusIndicator.PACKAGER_NAME}`;
+                this.packagerStatusItem.text = PackagerStatusIndicator.PACKAGER_STATUS_STOPPED;
+
+                this.togglePackagerItem.text = PackagerStatusIndicator.START_ICON;
                 this.togglePackagerItem.command = PackagerStatusIndicator.START_COMMAND;
                 this.togglePackagerItem.tooltip = localize("StartPackager", "Start Packager");
+
+                this.restartPackagerItem.command = PackagerStatusIndicator.RESTART_COMMAND;
                 break;
             case PackagerStatus.PACKAGER_STOPPING:
-                this.togglePackagerItem.text =  `${PackagerStatusIndicator.ACTIVITY_ICON} ${PackagerStatusIndicator.PACKAGER_NAME}`;
+                this.packagerStatusItem.text = PackagerStatusIndicator.PACKAGER_STATUS_STOPPING;
+
+                this.togglePackagerItem.text = PackagerStatusIndicator.ACTIVITY_ICON;
                 this.togglePackagerItem.command = undefined;
                 this.togglePackagerItem.tooltip = "";
+
+                this.restartPackagerItem.command = undefined;
                 break;
             case PackagerStatus.PACKAGER_STARTED:
-                this.togglePackagerItem.text =  `${PackagerStatusIndicator.STOP_ICON} ${PackagerStatusIndicator.PACKAGER_NAME}`;
+                this.packagerStatusItem.text = PackagerStatusIndicator.PACKAGER_STATUS_STARTED;
+
+                this.togglePackagerItem.text = PackagerStatusIndicator.STOP_ICON;
                 this.togglePackagerItem.command = PackagerStatusIndicator.STOP_COMMAND;
                 this.togglePackagerItem.tooltip = localize("StopPackager", "Stop Packager");
+
+                this.restartPackagerItem.command = PackagerStatusIndicator.RESTART_COMMAND;
                 break;
             case PackagerStatus.PACKAGER_STARTING:
-                this.togglePackagerItem.text =  `${PackagerStatusIndicator.ACTIVITY_ICON} ${PackagerStatusIndicator.PACKAGER_NAME}`;
+                this.packagerStatusItem.text = PackagerStatusIndicator.PACKAGER_STATUS_STARTING;
+
+                this.togglePackagerItem.text = PackagerStatusIndicator.ACTIVITY_ICON;
                 this.togglePackagerItem.command = undefined;
                 this.togglePackagerItem.tooltip = "";
+
+                this.restartPackagerItem.command = undefined;
                 break;
             default:
                 break;
