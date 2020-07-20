e52e2498b0fd63e609d140ac25c5eefe68a58e44
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-07-20 14:44:33 +0300

    Minor bugfix

--- src/common/packager.ts
+++ src/common/packager.ts
@@ -256,7 +256,7 @@ export class Packager {
                 if (stoppedOK) {
                     return this.start(true);
                 } else {
-                    return Promise.resolve<void>();
+                    return Promise.resolve();
                 }
             });
     }
