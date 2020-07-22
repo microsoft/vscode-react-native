89b7e46b0333b61e21a397197e3c96d796f7bf9c
Mikhail Suendukov <43934531+JiglioNero@users.noreply.github.com>
2020-07-22 11:27:25 +0300

    init LaunchScenariosManager

---
+++ src/extension/launchScenariosManager.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+export class LaunchScenariosManager {
+    private rootPath: string;
+
+    constructor(rootPath: string) {
+        this.rootPath = rootPath;
+    }
+}
