423e0f96f46d0946de3a70ee62c888e126021dc4
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-07-31 16:32:23 +0300

    Add BaseCDPMessageHandler class

---
+++ src/cdp-proxy/CDPMessageHandlers/baseCDPMessageHandler.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import { ICDPMessageHandler, ProcessedCDPMessage } from "./ICDPMessageHandler";
+
+export class BaseCDPMessageHandler implements ICDPMessageHandler {
+    constructor() {}
+
+    public processDebuggerCDPMessage(event: any): ProcessedCDPMessage {
+        return {
+            event,
+            sendBack: false,
+        };
+    }
+
+    public processApplicationCDPMessage(event: any): ProcessedCDPMessage {
+        return {
+            event,
+            sendBack: false,
+        };
+    }
+}
