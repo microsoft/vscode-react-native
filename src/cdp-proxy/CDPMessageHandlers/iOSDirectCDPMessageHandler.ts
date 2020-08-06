664e8b4e77ca0a9e9bbad40c016b113d12075449
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-08-06 13:56:19 +0300

    Add fixes for variables menu

--- src/cdp-proxy/CDPMessageHandlers/iOSDirectCDPMessageHandler.ts
+++ src/cdp-proxy/CDPMessageHandlers/iOSDirectCDPMessageHandler.ts
@@ -3,4 +3,13 @@
 
 import { BaseCDPMessageHandler } from "./baseCDPMessageHandler";
 export class IOSDirectCDPMessageHandler extends BaseCDPMessageHandler {
+    public processApplicationCDPMessage(event: any) {
+        if (event.result && event.result.properties) {
+            event.result = { result: event.result.properties};
+        }
+        return {
+            event,
+            sendBack: false,
+        };
+    }
 }
