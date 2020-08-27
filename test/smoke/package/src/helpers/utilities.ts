9b9a0bf96a7c9674a10545a13006285d24624dd9
Mikhail Suendukov <43934531+JiglioNero@users.noreply.github.com>
2020-08-27 17:23:17 +0300

    add logging

--- test/smoke/package/src/helpers/utilities.ts
+++ test/smoke/package/src/helpers/utilities.ts
@@ -168,6 +168,8 @@ export function filterProgressBarChars(str: string) {
 export function findStringInFile(filePath: string, strToFind: string): boolean {
     if (fs.existsSync(filePath)) {
         const content = fs.readFileSync(filePath).toString().trim();
+        console.log(filePath);
+        console.log(content);
         return content.includes(strToFind);
     }
     return false;
