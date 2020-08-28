c1d1a4ecddac83731b093dae3d99f922e43c275e
RedMickey <mihax2013@yandex.ru>
2020-08-28 10:29:15 +0300

    Add CDP handlers for Console and BreakpointsActive

--- src/cdp-proxy/CDPMessageHandlers/baseCDPMessageHandler.ts
+++ src/cdp-proxy/CDPMessageHandlers/baseCDPMessageHandler.ts
@@ -2,8 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import { ICDPMessageHandler, ProcessedCDPMessage } from "./ICDPMessageHandler";
+import { Connection } from "vscode-cdp-proxy";
 
 export abstract class BaseCDPMessageHandler implements ICDPMessageHandler {
+    protected debuggerTarget: Connection | null;
+    protected applicationTarget: Connection | null;
+
     constructor() {}
 
     public processDebuggerCDPMessage(event: any): ProcessedCDPMessage {
@@ -18,5 +22,13 @@ export abstract class BaseCDPMessageHandler implements ICDPMessageHandler {
             event,
             sendBack: false,
         };
+    }
+
+    public setDebuggerTarget(debuggerTarget: Connection | null): void {
+        this.debuggerTarget = debuggerTarget;
+    }
+
+    public setApplicationTarget(applicationTarget: Connection | null): void {
+        this.applicationTarget = applicationTarget;
     }
 }
--- src/cdp-proxy/CDPMessageHandlers/iOSDirectCDPMessageHandler.ts
+++ src/cdp-proxy/CDPMessageHandlers/iOSDirectCDPMessageHandler.ts
@@ -2,14 +2,61 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import { BaseCDPMessageHandler } from "./baseCDPMessageHandler";
+import { ProcessedCDPMessage } from "./ICDPMessageHandler";
+import { CDP_API_NAMES } from "./CDPAPINames";
+
 export class IOSDirectCDPMessageHandler extends BaseCDPMessageHandler {
-    public processApplicationCDPMessage(event: any) {
+    private isBackcompatConfigured: boolean;
+
+    constructor() {
+        super();
+        this.isBackcompatConfigured = false;
+    }
+
+    public processDebuggerCDPMessage(event: any): ProcessedCDPMessage {
+        let sendBack = false;
+        if (!this.isBackcompatConfigured && event.method === CDP_API_NAMES.RUNTIME_ENABLE) {
+            this.configureTargetForIWDPCommunication();
+        }
+        return {
+            event,
+            sendBack,
+        };
+    }
+
+    public processApplicationCDPMessage(event: any): ProcessedCDPMessage {
+        if (event.method === CDP_API_NAMES.CONSOLE_MESSAGE_ADDED) {
+            event = this.processDeprecatedConsoleMessage(event);
+        }
         if (event.result && event.result.properties) {
-            event.result = { result: event.result.properties};
+            event.result = { result: event.result.properties };
         }
         return {
             event,
             sendBack: false,
         };
+    }
+
+    private processDeprecatedConsoleMessage(event: any) {
+        return {
+            method: CDP_API_NAMES.RUNTIME_CONSOLE_API_CALLED,
+            params: {
+                type: event.params.message.type,
+                timestamp: event.params.message.timestamp,
+                args: event.params.message.parameters || [{ type: "string", value: event.params.message.text }],
+                stackTrace: { callFrames: event.params.message.stack || event.params.message.stackTrace },
+                executionContextId: 1,
+            },
+        };
+    }
+
+    private configureTargetForIWDPCommunication(): void {
+        this.isBackcompatConfigured = true;
+        try {
+            this.applicationTarget?.api.Console.enable({});
+            this.applicationTarget?.api.Debugger.setBreakpointsActive({ active: true });
+        } catch (err) {
+            // Specifically ignore a fail here since it's only for backcompat
+        }
     }
 }
