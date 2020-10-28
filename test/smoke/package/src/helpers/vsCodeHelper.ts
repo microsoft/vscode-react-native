6967df44802694150fe004c5dc028bc0782d94ab
JiglioNero <43934531+JiglioNero@users.noreply.github.com>
2020-10-28 09:48:03 +0300

    use vscode-test for vscode client download (#1443)

--- test/smoke/package/src/helpers/vsCodeHelper.ts
+++ test/smoke/package/src/helpers/vsCodeHelper.ts
@@ -1,64 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-import * as remote from "gulp-remote-src-vscode";
-import * as vzip from "gulp-vinyl-zip";
-import * as vfs from "vinyl-fs";
-import * as untar from "gulp-untar";
-import * as gunzip from "gulp-gunzip";
-import * as chmod from "gulp-chmod";
-import * as filter from "gulp-filter";
 import * as path from "path";
 import * as utilities from "./utilities";
-import * as request from "request";
-import * as source from "vinyl-source-stream";
 import * as rimraf from "rimraf";
 import * as cp from "child_process";
+import * as vscodeTest from "vscode-test";
 
 import { spawnSync } from "./utilities";
 
 export class VSCodeHelper {
     private static version;
-    private static isInsiders;
     private static downloadPlatform = (process.platform === "darwin") ? "darwin" : process.platform === "win32" ? "win32-x64-archive" : "linux-x64";
     private static artifactsFolderName = "drop-win";
 
-    public static async downloadVSCodeExecutable(targetFolder: string): Promise<any> {
+    public static async downloadVSCodeExecutable(): Promise<string> {
         VSCodeHelper.version = process.env.CODE_VERSION || "*";
-        VSCodeHelper.isInsiders = VSCodeHelper.version === "insiders";
-        const testRunFolder = path.join(targetFolder, ".vscode-test", VSCodeHelper.isInsiders ? "insiders" : "stable");
 
-        return new Promise ((resolve) => {
-            VSCodeHelper.getDownloadUrl((downloadUrl) => {
-                console.log("*** Downloading VS Code into \"" + testRunFolder + "\" from: " + downloadUrl);
-
-                let version = downloadUrl.match(/\d+\.\d+\.\d+/)[0].split("\.");
-                let isTarGz = downloadUrl.match(/linux/) && version[0] >= 1 && version[1] >= 5;
-
-                let stream;
-                if (isTarGz) {
-                    let gulpFilter = filter(["VSCode-linux-x64/bin/*", "VSCode-linux-x64/code", "VSCode-linux-x64/code-insiders", "VSCode-linux-x64/resources/app/node_modules*/vscode-ripgrep/**/rg"], { restore: true });
-                    stream = request(utilities.toRequestOptions(downloadUrl))
-                        .pipe(source(path.basename(downloadUrl)))
-                        .pipe(gunzip())
-                        .pipe(untar())
-                        .pipe(gulpFilter)
-                        .pipe(chmod(493)) // 0o755
-                        .pipe(gulpFilter.restore)
-                        .pipe(vfs.dest(testRunFolder));
-                } else {
-                    stream = remote("", { base: downloadUrl })
-                        .pipe(vzip.src())
-                        .pipe(vfs.dest(testRunFolder));
-                }
-                stream.on("end", () => {
-                    resolve();
-                });
-            });
-        });
+        return vscodeTest.downloadAndUnzipVSCode(VSCodeHelper.version, VSCodeHelper.downloadPlatform);
     }
 
-    public static installExtensionFromVSIX(extensionDir: string, testVSCodeExecutablePath: string, resourcesPath: string, deleteVSIX: boolean) {
+    public static installExtensionFromVSIX(extensionDir: string, testVSCodeExecutablePath: string, resourcesPath: string, deleteVSIX: boolean): void {
         let args: string[] = [];
         args.push(`--extensions-dir=${extensionDir}`);
         const artifactPath = path.join(resourcesPath, VSCodeHelper.artifactsFolderName);
@@ -78,6 +40,10 @@ export class VSCodeHelper {
         } else {
             console.log("*** --dont-delete-vsix parameter is set, skipping deleting of VSIX");
         }
+    }
+
+    public static getVSCodeExecutablePath(testVSCodeFolder: string): string {
+        return vscodeTest.resolveCliPathFromVSCodeExecutablePath(testVSCodeFolder);
     }
 
     public static killWinCodeProcesses(taskKillCommands: string[]): void {
@@ -118,35 +84,5 @@ export class VSCodeHelper {
         // CodeHelper.exe window
         commands.push(`taskkill /f /t /fi "IMAGENAME eq CodeHelper.exe" /fi "USERNAME eq ${userName}`);
         return commands;
-    }
-
-    private static getDownloadUrl(cb) {
-        VSCodeHelper.getTag(function (tag) {
-            // TODO: Update download endpoint azurewebsites.net -> visualstudio.com (https://github.com/microsoft/vscode-test/blob/b8813110b229fa1a524650c16ec521df42b7893d/lib/util.ts#L23)
-            return cb(["https://vscode-update.azurewebsites.net", tag, VSCodeHelper.downloadPlatform, (VSCodeHelper.isInsiders ? "insider" : "stable")].join("/"));
-        });
-    }
-
-    private static getTag(cb) {
-        if (VSCodeHelper.version !== "*" && VSCodeHelper.version !== "insiders") {
-            return cb(VSCodeHelper.version);
-        }
-
-        utilities.getContents("https://vscode-update.azurewebsites.net/api/releases/" + (VSCodeHelper.isInsiders ? "insider/" : "stable/") + VSCodeHelper.downloadPlatform, null, null, function (error, tagsRaw) {
-            if (error) {
-                VSCodeHelper.exitWithError(error);
-            }
-
-            try {
-                cb(JSON.parse(tagsRaw)[0]); // first one is latest
-            } catch (error) {
-                VSCodeHelper.exitWithError(error);
-            }
-        });
-    }
-
-    private static exitWithError(error) {
-        console.error("Error while downloading VS Code: " + error.toString());
-        process.exit(1);
     }
 }
