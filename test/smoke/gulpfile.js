41d17648c4b095333ab443464391503a601e83dc
RedMickey <33267199+RedMickey@users.noreply.github.com>
2021-02-10 14:06:22 +0300

    Update Eslint dependencies (#1500)

--- test/smoke/gulpfile.js
+++ test/smoke/gulpfile.js
@@ -27,11 +27,14 @@ const runPrettier = (onlyStaged, fix, callback) => {
 };
 
 const runEslint = (fix, callback) => {
-    const child = cp.fork(
-        "../../node_modules/eslint/bin/eslint.js",
-        ["--color", "package/src/**/*.ts", "automation/src/**/*.ts", fix ? "--fix" : ""],
-        { stdio: "inherit" },
-    );
+    let args = ["--color", "package/src/**/*.ts", "automation/src/**/*.ts"];
+    if (fix) {
+        args.push("--fix");
+    }
+    const child = cp.fork("../../node_modules/eslint/bin/eslint.js", args, {
+        stdio: "inherit",
+        cwd: __dirname,
+    });
 
     child.on("exit", code => (code ? callback(`Eslint exited with code ${code}`) : callback()));
 };
