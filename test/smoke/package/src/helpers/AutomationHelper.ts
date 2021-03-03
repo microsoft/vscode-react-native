11090e7fe2a71944baa9337fe90e3eccced5af28
JiglioNero <smouk.chayz@gmail.com>
2021-03-03 14:57:10 +0300

    fix

--- test/smoke/package/src/helpers/AutomationHelper.ts
+++ test/smoke/package/src/helpers/AutomationHelper.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
+import { executeWithSpecifiedPollRetryParameters } from "../../../automation/src/code";
 import { Application } from "../../../automation";
 import { SmokeTestsConstants } from "./smokeTestsConstants";
 
@@ -22,7 +23,7 @@ export default class AutomationHelper {
         let tryes = retryCount;
         while (tryes > 0) {
             try {
-                await this.app.workbench.code.executeWithSpecifiedPollRetryParameters(
+                await executeWithSpecifiedPollRetryParameters(
                     fun,
                     pollRetryCount,
                     pollRetryInterval,
