4f8866f2c3b55f9374de80aee9bae29ecb91c4ba
JiglioNero <43934531+JiglioNero@users.noreply.github.com>
2021-06-11 16:47:26 +0300

    Update test/common/packageLoader.test.ts
    Co-authored-by: RedMickey <33267199+RedMickey@users.noreply.github.com>

--- test/common/packageLoader.test.ts
+++ test/common/packageLoader.test.ts
@@ -156,7 +156,7 @@ suite("packageLoader", async () => {
             tryToRequireAfterInstallStub.restore();
         });
 
-        test("The package loader should install package with specific version if the package already installed but with another version", async function () {
+        test("The package loader should install a package with the specific version if the package is already installed but with another version", async function () {
             this.timeout(packageLoaderTestTimeout);
 
             await commandExecutor.spawn(HostPlatform.getNpmCliCommand("npm"), [
