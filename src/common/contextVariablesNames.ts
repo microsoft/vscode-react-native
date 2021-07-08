e055a25d866ab5dc04a1d8c163408476ccda0f14
RedMickey <33267199+RedMickey@users.noreply.github.com>
2021-07-08 11:45:18 +0300

    Update src/common/contextVariablesNames.ts
    Co-authored-by: etatanova <77284023+etatanova@users.noreply.github.com>

--- src/common/contextVariablesNames.ts
+++ src/common/contextVariablesNames.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 export const CONTEXT_VARIABLES_NAMES = {
-    IS_RN_PACKAGER_RUNNING: "isRnPackagerRunning",
+    IS_RN_PACKAGER_RUNNING: "isRNPackagerRunning",
     IS_RNT_NETWORK_INSPECTOR_RUNNING: "isRNTNetworkInspectorRunning",
     IS_RN_WINDOWS_PROJECT: "isRNWindowsProject",
     IS_RN_MACOS_PROJECT: "isRNMacOSProject",
