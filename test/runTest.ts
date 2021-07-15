22a3ea781efafeb42365f516a1fb1d6e269661f7
RedMickey <mihax2013@yandex.ru>
2021-07-15 13:58:51 +0300

    test6

--- test/runTest.ts
+++ test/runTest.ts
@@ -19,7 +19,7 @@ async function launchTests() {
         await runTests({
             extensionDevelopmentPath,
             extensionTestsPath,
-            version: "1.52.0",
+            version: "1.58.1",
         });
     } catch (err) {
         console.error(err);
