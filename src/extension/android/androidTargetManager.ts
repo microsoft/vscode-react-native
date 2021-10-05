d7a5c31c8f8ea491e35ed622112eedba77fd9f0f
JiglioNero <43934531+JiglioNero@users.noreply.github.com>
2021-10-05 14:50:52 +0300

    Update src/extension/android/androidTargetManager.ts
    Co-authored-by: RedMickey <33267199+RedMickey@users.noreply.github.com>

--- src/extension/android/androidTargetManager.ts
+++ src/extension/android/androidTargetManager.ts
@@ -57,7 +57,7 @@ export class AndroidTargetManager extends MobileTargetManager {
                 if (onlineTarget) {
                     return onlineTarget.isVirtualTarget;
                 } else {
-                    throw Error("There is no any online target");
+                    throw new Error("There is no any online target");
                 }
             }
         } catch {
