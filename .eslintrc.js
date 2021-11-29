c3fb83b72dfc210ad4cded40d81f0d452b9102c9
Heniker <art_saif@mail.ru>
2021-11-29 16:18:29 +0300

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,4 @@
-/*
-static properties
-
-static fn
-
-all definitions
-
-constructor
-
-public
-
-private
-
-
-
-exclude en-extension.
-*/
+const prettierConfig = require("./package.json").prettier;
 
 module.exports = {
     ignorePatterns: ["**/*.d.ts", "**/*.js"],
@@ -31,7 +15,7 @@ module.exports = {
         sourceType: "module",
         project: "./tsconfig.json",
         // ecmaFeatures: {
-        //   jsx: true,
+        //   jsx: false,
         // },
     },
     plugins: ["@typescript-eslint", "prettier", "import", "promise", "unicorn", "header"],
@@ -59,51 +43,43 @@ module.exports = {
     },
     overrides: [],
     rules: {
-        "unicorn/prefer-ternary": "warn",
-        "import/no-cycle": "off",
-        "import/no-unresolved": "off", // !
-        "lines-between-class-members": "off",
-        "@typescript-eslint/lines-between-class-members": "off",
-        "header/header": [
-            "error",
-            "line",
-            [
-                " Copyright (c) Microsoft Corporation. All rights reserved.",
-                " Licensed under the MIT license. See LICENSE file in the project root for details.",
-            ],
-        ],
-        "@typescript-eslint/no-unsafe-assignment": "off", // ?
-        "@typescript-eslint/no-unsafe-member-access": "off", // ?
-        // '@typescript-eslint/no-unsafe-return': ['off'],
-        // 'class-methods-use-this': 'off',
-        // 'consistent-return': 'off',
-        // 'spaced-comment': ['off'],
-        "no-restricted-syntax": [
-            "error",
-            {
-                selector: "ForInStatement",
-                message:
-                    "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
-            },
-            {
-                selector: "LabeledStatement",
-                message:
-                    "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
-            },
-            {
-                selector: "WithStatement",
-                message:
-                    "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
-            },
-        ],
-        "no-void": ["error", { allowAsStatement: true }], // interferes with @typescript-eslint/no-floating-promises
-        "@typescript-eslint/explicit-function-return-type": ["off"], // too much hassle, too little value
+        "promise/catch-or-return": "off",
+        // too much hassle, too little value
+        "@typescript-eslint/no-unnecessary-type-assertion": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        // '@typescript-eslint/member-ordering': [
+        //   'warn',
+        //   {
+        //     classes: {
+        //       memberTypes: [
+        //         'static-field',
+        //         'static-method',
+        //         'instance-field',
+        //         'constructor',
+        //         'public-instance-method',
+        //         'protected-instance-method',
+        //         'private-instance-method',
+        //       ],
+        //     },
+        //   },
+        // ],
         "@typescript-eslint/explicit-module-boundary-types": [
             "warn",
             {
                 allowArgumentsExplicitlyTypedAsAny: true,
             },
         ],
+        "@typescript-eslint/lines-between-class-members": "off",
+        "@typescript-eslint/no-inferrable-types": [
+            "warn",
+            {
+                ignoreParameters: true,
+                ignoreProperties: false,
+            },
+        ],
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-use-before-define": [
             // function hoisting is a common, accepted pattern
             "error",
@@ -113,6 +89,17 @@ module.exports = {
                 typedefs: true,
                 variables: true,
             },
+        ],
+        // disallow function implicitly returning any
+        "class-methods-use-this": "off",
+        "consistent-return": "warn",
+        "header/header": [
+            "error",
+            "line",
+            [
+                " Copyright (c) Microsoft Corporation. All rights reserved.",
+                " Licensed under the MIT license. See LICENSE file in the project root for details.",
+            ],
         ],
         "import/extensions": [
             "error",
@@ -125,45 +112,74 @@ module.exports = {
                 tsx: "never",
             },
         ],
+        "import/no-cycle": "off",
+        "import/no-unresolved": "off",
         "import/order": "warn",
-        "import/prefer-default-export": "off", // default exports are bad
-        "linebreak-style": "off", // git auto-fixes this
-        "max-classes-per-file": "off", // eslint considers too many things to be a class
-        "no-nested-ternary": "off", // superseeded by unicorn/no-nested-ternary
-        "no-plusplus": "off", // ?
-        "no-underscore-dangle": "off", // ?
-        "prefer-destructuring": "off", // ?
-        // "prettier/prettier": [
-        //     "error",
-        //     {
-        //         trailingComma: "all",
-        //         arrowParens: "avoid",
-        //         printWidth: 100,
-        //         tabWidth: 4,
-        //         endOfLine: "auto",
-        //         overrides: [
-        //             {
-        //                 files: ["*.md"],
-        //                 options: {
-        //                     tabWidth: 2,
-        //                     printWidth: 80,
-        //                 },
-        //             },
-        //         ],
-        //     },
-        // ],
-        quotes: ["error", "double"], // single or double - choose either, but this is a required rule
-        // semi: ["error", "always"], //
-        "unicorn/filename-case": [
+        // default exports are bad
+        "import/prefer-default-export": "off",
+        // git auto-fixes this
+        "linebreak-style": "off",
+        "lines-between-class-members": "off",
+        // eslint considers too many things to be a class
+        "max-classes-per-file": "off",
+        // superseeded by unicorn/no-nested-ternary
+        "no-nested-ternary": "off",
+        "no-plusplus": "off",
+        "no-restricted-syntax": [
             "error",
             {
-                cases: {
-                    camelCase: true,
-                    // pascalCase: true,
-                },
+                message:
+                    "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+                selector: "ForInStatement",
+            },
+            {
+                message:
+                    "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+                selector: "LabeledStatement",
+            },
+            {
+                message:
+                    "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+                selector: "WithStatement",
             },
         ],
-        "unicorn/no-useless-undefined": "off", // interferes with typescript best practices
+        "no-underscore-dangle": "off",
+        // interferes with @typescript-eslint/no-floating-promises
+        "no-void": [
+            "error",
+            {
+                allowAsStatement: true,
+            },
+        ],
+        "prefer-destructuring": "off",
+        "prettier/prettier": ["error", prettierConfig],
+        "no-shadow": "off",
+        "@typescript-eslint/no-shadow": "off",
+        quotes: "off",
+        "@typescript-eslint/quotes": [
+            "error",
+            "double",
+            { avoidEscape: true, allowTemplateLiterals: true },
+        ],
+        "spaced-comment": "warn",
+        "no-param-reassign": "warn",
+        "@typescript-eslint/no-unsafe-call": "off", // !
+        semi: ["error", "always"],
+        "promise/param-names": "warn",
+        "unicorn/no-array-push-push": "warn",
+        "unicorn/filename-case": [
+            "warn",
+            {
+                cases: {
+                    camelCase: true, // pascalCase: true,
+                },
+                ignore: [/rn-extension\.ts/],
+            },
+        ],
+        "unicorn/no-useless-undefined": "off",
+        "unicorn/prefer-ternary": "warn",
+        "unicorn/no-null": "warn",
+        // interferes with typescript best practices
         "unicorn/prevent-abbreviations": "off", // most abbreviations are well-known
     },
 
