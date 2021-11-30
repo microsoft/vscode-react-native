a3a76261a51339d2a7c655849a0495656c4fb66e
Heniker <art_saif@mail.ru>
2021-11-30 23:14:08 +0300

    disable eslint for broken file

--- src/common/telemetry.ts
+++ src/common/telemetry.ts
@@ -6,6 +6,8 @@ import * as fs from "fs";
 import * as path from "path";
 import { HostPlatform } from "./hostPlatform";
 
+/* eslint-disable */
+
 /**
  * Telemetry module specialized for vscode integration.
  */
