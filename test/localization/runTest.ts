6759ec547e8cdda10312bfe1c645b7906a8b4a14
RedMickey <mihax2013@yandex.ru>
2021-12-03 16:31:04 +0300

    Bump VS Code version to 1.62.3 in unit tests

--- test/localization/runTest.ts
+++ test/localization/runTest.ts
@@ -20,7 +20,7 @@ async function launchTests() {
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: ["--locale", "ru"],
-            version: "1.61.0",
+            version: "1.62.3",
         });
     } catch (err) {
         console.error(err);
