b79d30bc8d9973414f3cbffe8952f700cdff1fb8
Samriel <andrei.lobanovic@akvelon.com>
2022-02-17 11:03:48 +0300

    minor fix

--- src/common/node/promise.ts
+++ src/common/node/promise.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-// import { cat } from "shelljs";
 import { CancellationTokenSource, Disposable } from "vscode";
 
 /**
