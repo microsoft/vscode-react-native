255b035e80625797ee4cf47a63caf2faffb36026
Heniker <art_saif@mail.ru>
2022-02-22 18:01:08 +0300

    fix eslint

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,14 @@
-interface Thenable<T> {
-    then<TResult>(
-        this: void,
-        onfulfilled?: (value: T) => TResult | Thenable<TResult>,
-        onrejected?: (reason: any) => TResult | Thenable<TResult>,
-    ): Thenable<TResult>;
-    then<TResult>(
-        this: void,
-        onfulfilled?: (value: T) => TResult | Thenable<TResult>,
-        onrejected?: (reason: any) => void,
-    ): Thenable<TResult>;
+declare module "vscode" {
+    interface Thenable<T> {
+        then<TResult>(
+            this: void,
+            onfulfilled?: (value: T) => TResult | Thenable<TResult>,
+            onrejected?: (reason: any) => TResult | Thenable<TResult>,
+        ): Thenable<TResult>;
+        then<TResult>(
+            this: void,
+            onfulfilled?: (value: T) => TResult | Thenable<TResult>,
+            onrejected?: (reason: any) => void,
+        ): Thenable<TResult>;
+    }
 }
