530e1bea7610472898d2fa0d0fd52d4b5bfc8349
JiglioNero <smouk.chayz@gmail.com>
2022-03-23 01:04:32 +0100

    remove reduntant logging

--- test/smoke/package/src/helpers/launchConfigurationManager.ts
+++ test/smoke/package/src/helpers/launchConfigurationManager.ts
@@ -57,7 +57,6 @@ export class LaunchConfigurationManager {
     public readLaunchScenarios(): ILaunchScenarios {
         if (fs.existsSync(this.pathToLaunchFile)) {
             const content = fs.readFileSync(this.pathToLaunchFile, "utf8");
-            console.log("lounch.json content:", content);
             this.launchScenarios = JSON.parse(stripJsonComments(content));
         }
         return this.launchScenarios;
