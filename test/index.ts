3030cc611b2fc87c2ee04918db3bf5a12a17d2bf
Ezio Li <v-yukl@microsoft.com>
2022-06-27 10:57:08 +0800

    test

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
+
 // This file is used by VS Code's default test runner to configure Mocha before the test run.
 
 import * as path from "path";
