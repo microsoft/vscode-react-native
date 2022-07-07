bad42581635d7205cd06500c70ee773a258eca7d
EzioLi <v-yukl@microsoft.com>
2022-07-07 10:11:15 +0800

    Update

--- src/extension/ios/plistBuddy.ts
+++ src/extension/ios/plistBuddy.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
+
 import * as path from "path";
 import * as fs from "fs";
 import * as glob from "glob";
