20256568b3e40caee16e3e5018b0587468df4171
EzioLi <v-yukl@microsoft.com>
2023-10-31 16:15:07 +0800

    Update

--- src/common/installHelper.ts
+++ src/common/installHelper.ts
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-// import * as fs from "fs";
-// import * as https from "https";
-// import * as vscode from "vscode";
 import { AdbHelper } from "../extension/android/adb";
 import { AppLauncher } from "../extension/appLauncher";
 import { OutputChannelLogger } from "../extension/log/OutputChannelLogger";
