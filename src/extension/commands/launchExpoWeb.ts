2f6063a8cb3fec20147eb07f338167a53b71b907
benjaminbi <bi.crystal@gmail.com>
2023-11-23 11:24:22 +0800

    add verification of Expo project

--- src/extension/commands/launchExpoWeb.ts
+++ src/extension/commands/launchExpoWeb.ts
@@ -6,10 +6,14 @@ import { ErrorHelper } from "../../common/error/errorHelper";
 import { InternalErrorCode } from "../../common/error/internalErrorCode";
 import { PlatformType } from "../launchArgs";
 import { AppLauncher } from "../appLauncher";
-// import { GeneralPlatform } from "../generalPlatform";
+import { OutputChannelLogger } from "../log/OutputChannelLogger";
 import { getRunOptions } from "./util";
 import { Command } from "./util/command";
 import { ExponentPlatform } from "../exponent/exponentPlatform";
+import * as nls from "vscode-nls";
+
+const localize = nls.loadMessageBundle();
+const logger = OutputChannelLogger.getMainChannel();
 
 export class launchExpoWeb extends Command {
     codeName = "launchExpoWeb";
@@ -18,6 +22,13 @@ export class launchExpoWeb extends Command {
 
     async baseFn(launchArgs: any): Promise<any> {
         assert(this.project);
+        const expoHelper = this.project.getExponentHelper();
+        logger.info(localize("CheckExpoEnvironment", "Checking Expo project environment."));
+        const isExpo = await expoHelper.isExpoManagedApp(true);
+        if (isExpo == false) {
+            logger.info(localize("NotAnExpoProject", "This is not an Expo project."));
+            return;
+        }
         await runExpoWeb(this.project);
     }
 }
@@ -31,4 +42,3 @@ async function runExpoWeb(project: AppLauncher) {
     await platform.beforeStartPackager();
     await platform.startPackager();
 }
-
