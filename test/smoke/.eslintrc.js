e3e98510d94dfaf2ec660bc6ef765f2971b7d118
EzioLi <v-yukl@microsoft.com>
2024-02-01 20:35:30 +0800

    Fix security vulnerabilities

--- test/smoke/.eslintrc.js
+++ test/smoke/.eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/prefer-namespace-keyword": "off",
         "@typescript-eslint/triple-slash-reference": "off",
         "header/header": [
--- test/smoke/package/src/debugMacOS.test.ts
+++ test/smoke/package/src/debugMacOS.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-import { Application } from "../../automation";
+import { Application } from "../../automation/src";
 import * as assert from "assert";
 import * as cp from "child_process";
 import { SmokeTestsConstants } from "./helpers/smokeTestsConstants";
