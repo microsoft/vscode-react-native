583c3a7eb3e97a4d28fec0b8c5747027ab72e073
Ezio Li <v-yukl@microsoft.com>
2024-02-05 14:33:22 +0800

    Fix security vulnerabilities (#2104)

--- test/smoke/package/src/helpers/AutomationHelper.ts
+++ test/smoke/package/src/helpers/AutomationHelper.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-import { Application } from "../../../automation";
+import { Application } from "../../../automation/src";
 import { SmokeTestsConstants } from "./smokeTestsConstants";
 
 export interface IStackFrame {
