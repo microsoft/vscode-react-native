484cc18d3070cf1a83091f044307f6e7255b9e19
Ezio Li <v-yukl@microsoft.com>
2024-08-20 19:38:01 +0800

    Update extension localization strings - Part1 (#2189)
    * Update extension localization strings
    
    * String update
    
    * Update

--- src/extension/services/validationService/checks/xcodecli.ts
+++ src/extension/services/validationService/checks/xcodecli.ts
@@ -9,7 +9,7 @@ nls.config({
     messageFormat: nls.MessageFormat.bundle,
     bundleFormat: nls.BundleFormat.standalone,
 })();
-const toLocale = nls.loadMessageBundle();
+const locale = nls.loadMessageBundle();
 
 const label = "Xcode CLI";
 
@@ -31,8 +31,8 @@ async function test(): Promise<ValidationResultT> {
 const main: IValidation = {
     label,
     platform: ["darwin"],
-    description: toLocale(
-        "xcodeCLICheckDescription",
+    description: locale(
+        "XcodeCLICheckDescription",
         "Required for building and testing RN macOS apps",
     ),
     category: ValidationCategoryE.macOS,
