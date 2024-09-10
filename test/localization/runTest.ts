e6592ca0244bbca696ff564e42fc978df11139e9
lexie011 <liudandan11@beyondsoft.com>
2024-09-10 10:02:10 +0800

    Switch Vscode back to stable version to run localization test

--- test/localization/runTest.ts
+++ test/localization/runTest.ts
@@ -20,7 +20,7 @@ async function launchTests() {
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: ["--locale", "zh-cn"],
-            version: "1.91.1",
+            version: "stable",
         });
     } catch (err) {
         console.error(err);
