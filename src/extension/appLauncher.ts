709232a60fffc013041c56abbe6ac6062ab52208
lexie011 <liudandan11@beyondsoft.com>
2025-02-24 09:40:19 +0800

    Fix debug issue with Expo web

--- src/extension/appLauncher.ts
+++ src/extension/appLauncher.ts
@@ -515,7 +515,6 @@ export class AppLauncher {
                 this.browserProc = child_process.spawn(browserPath.path, runArguments, {
                     detached: true,
                     stdio: ["ignore"],
-                    shell: true,
                 });
                 this.browserProc.unref();
                 this.browserProc.on("error", err => {
