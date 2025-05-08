63acd8608774e53f855629d083233cbbd1cd068b
EzioLi <v-yukl@microsoft.com>
2025-05-08 10:19:53 +0800

    Fix CodeQL: [SM04580] Server side request forgery

--- src/common/node/request.ts
+++ src/common/node/request.ts
@@ -7,7 +7,7 @@ import https = require("https");
 export class Request {
     public static request(url: string, expectStatusOK = false, isHttps = false): Promise<string> {
         return new Promise((resolve, reject) => {
-            const req = (isHttps ? https : http).get(url, res => {
+            const req = (isHttps ? https : http).get(url, res => { // CodeQL [SM04580] extension send all requests to 'localhost:port' which are built by local application
                 let responseString = "";
                 res.on("data", (data: Buffer) => {
                     responseString += data.toString();
