e72b3020808ece27ac6fb1ba31f384623d4fe8b5
EzioLi <v-yukl@microsoft.com>
2025-06-19 09:52:29 +0800

    [DO NOT REVIEW] Test vscode performance

--- test/extension/mobileTargetManager.test.ts
+++ test/extension/mobileTargetManager.test.ts
@@ -469,74 +469,66 @@ suite("MobileTargetManager", function () {
             runTargetSelectionTests();
         });
 
-        suite.skip(
-            "Collect targets in case there is no 'emulator' utility in the PATH",
-            function () {
-                suiteSetup(() => {
-                    setupWithEmulatorCommands();
-                });
+        suite("Collect targets in case there is no 'emulator' utility in the PATH", function () {
+            suiteSetup(() => {
+                setupWithEmulatorCommands();
+            });
 
-                suiteTeardown(() => {
-                    defaultSetup();
-                });
+            suiteTeardown(() => {
+                defaultSetup();
+            });
 
-                test(`Should not throw an error and collect only online targets in case the passed target type is undefined`, async function () {
-                    await executeWithoutEmulator(async () => {
-                        try {
-                            await targetManager.collectTargets();
-                            assert.strictEqual(
-                                (await targetManager.getTargetList()).find(
-                                    target => !target.isOnline,
-                                ),
-                                undefined,
-                                "Should collect only online targets",
-                            );
-                        } catch (error) {
-                            assert.fail(`Error has been thrown: ${error}`);
-                        }
-                    });
-                }).timeout(10000);
-                test(`Should not throw an error in case the passed target type equals '${TargetType.Device}'`, async function () {
-                    await executeWithoutEmulator(async () => {
-                        try {
-                            await targetManager.collectTargets(TargetType.Device);
-                            assert.strictEqual(
-                                (await targetManager.getTargetList()).find(
-                                    target => target.isVirtualTarget,
-                                ),
-                                undefined,
-                                "Should collect only devices",
-                            );
-                        } catch (error) {
-                            assert.fail(`Error has been thrown: ${error}`);
-                        }
-                    });
+            test(`Should not throw an error and collect only online targets in case the passed target type is undefined`, async function () {
+                await executeWithoutEmulator(async () => {
+                    try {
+                        await targetManager.collectTargets();
+                        assert.strictEqual(
+                            (await targetManager.getTargetList()).find(target => !target.isOnline),
+                            undefined,
+                            "Should collect only online targets",
+                        );
+                    } catch (error) {
+                        assert.fail(`Error has been thrown: ${error}`);
+                    }
                 });
-                test(`Should throw an error in case the passed target type equals '${TargetType.Simulator}'`, async function () {
-                    await executeWithoutEmulator(async () => {
-                        try {
-                            await targetManager.collectTargets(TargetType.Simulator);
-                            assert.fail(`Did not throw error.`);
-                        } catch (error) {
-                            if (error instanceof InternalError) {
-                                assert.strictEqual(
-                                    error.errorCode,
-                                    InternalErrorCode.CommandFailed,
-                                );
-                            } else {
-                                throw error;
-                            }
-                        }
-                    });
+            }).timeout(10000);
+            test(`Should not throw an error in case the passed target type equals '${TargetType.Device}'`, async function () {
+                await executeWithoutEmulator(async () => {
+                    try {
+                        await targetManager.collectTargets(TargetType.Device);
+                        assert.strictEqual(
+                            (await targetManager.getTargetList()).find(
+                                target => target.isVirtualTarget,
+                            ),
+                            undefined,
+                            "Should collect only devices",
+                        );
+                    } catch (error) {
+                        assert.fail(`Error has been thrown: ${error}`);
+                    }
                 });
-            },
-        );
+            });
+            test(`Should throw an error in case the passed target type equals '${TargetType.Simulator}'`, async function () {
+                await executeWithoutEmulator(async () => {
+                    try {
+                        await targetManager.collectTargets(TargetType.Simulator);
+                        assert.fail(`Did not throw error.`);
+                    } catch (error) {
+                        if (error instanceof InternalError) {
+                            assert.strictEqual(error.errorCode, InternalErrorCode.CommandFailed);
+                        } else {
+                            throw error;
+                        }
+                    }
+                });
+            });
+        });
 
         suite("Target identification", function () {
             runTargetTypeCheckTests();
         });
 
-        suite.skip(
+        suite(
             "Target identification in case there is no 'emulator' utility in the PATH",
             function () {
                 suiteSetup(() => {
