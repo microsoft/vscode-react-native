62116eafed6019a5c079087011b8dafc3f320211
EmmaYuan1015 <EmmaYuan1015@users.noreply.github.com>
2026-08-13 10:04:28 +0800

    Fix async Promise executors

--- src/extension/networkInspector/networkInspectorServer.ts
+++ src/extension/networkInspector/networkInspectorServer.ts
@@ -78,26 +78,21 @@ export class NetworkInspectorServer {
             false,
             "networkInspectorLogsColorTheme",
         );
-        this.initialisePromise = new Promise(async (resolve, reject) => {
-            this.certificateProvider = new CertificateProvider(adbHelper);
-
-            try {
-                let options = await this.certificateProvider.loadSecureServerConfig();
-                this.secureServer = await this.startServer(
-                    NetworkInspectorServer.SecureServerPort,
-                    options,
-                );
-                this.insecureServer = await this.startServer(
-                    NetworkInspectorServer.InsecureServerPort,
-                );
-            } catch (err) {
-                return reject(err);
-            }
-
-            this.logger.info(localize("NetworkInspectorWorking", "Network inspector is working"));
-            resolve();
-        });
+        this.initialisePromise = this.initialize(adbHelper);
         return this.initialisePromise;
+    }
+
+    private async initialize(adbHelper: AdbHelper): Promise<void> {
+        this.certificateProvider = new CertificateProvider(adbHelper);
+
+        const options = await this.certificateProvider.loadSecureServerConfig();
+        this.secureServer = await this.startServer(
+            NetworkInspectorServer.SecureServerPort,
+            options,
+        );
+        this.insecureServer = await this.startServer(NetworkInspectorServer.InsecureServerPort);
+
+        this.logger.info(localize("NetworkInspectorWorking", "Network inspector is working"));
     }
 
     public async stop(): Promise<void> {
